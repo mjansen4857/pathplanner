6413b61a1b6b130eba6a39021a4a6159af47a5cd
Michael Jansen <mjansen4857@gmail.com>
2018-04-25 09:43:55 -0400

    Drag points and edit with table

--- src/org/rangerrobotics/pathplanner/geometry/Vector2.java
+++ src/org/rangerrobotics/pathplanner/geometry/Vector2.java
@@ -1,23 +1,42 @@
 package org.rangerrobotics.pathplanner.geometry;
 
-public class Vector2 {
-    public double x;
-    public double y;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class Vector2 extends RecursiveTreeObject<Vector2>{
+    public SimpleDoubleProperty x;
+    public SimpleDoubleProperty y;
 
     public Vector2(double x, double y){
-        this.x = x;
-        this.y = y;
+        this.x = new SimpleDoubleProperty(x);
+        this.y = new SimpleDoubleProperty(y);
+    }
+
+    public double getX() {
+        return x.get();
+    }
+
+    public double getY() {
+        return y.get();
+    }
+
+    public void setX(double x) {
+        this.x.set(x);
+    }
+
+    public void setY(double y) {
+        this.y.set(y);
     }
 
     public static Vector2 add(Vector2 a, Vector2 b){
-        return new Vector2(a.x + b.x, a.y + b.y);
+        return new Vector2(a.getX() + b.getX(), a.getY() + b.getY());
     }
 
     public static Vector2 subtract(Vector2 a, Vector2 b){
-        return new Vector2(a.x - b.x, a.y - b.y);
+        return new Vector2(a.getX() - b.getX(), a.getY() - b.getY());
     }
 
     public static Vector2 multiply(Vector2 vec, double mult){
-        return new Vector2(vec.x * mult, vec.y * mult);
+        return new Vector2(vec.getX() * mult, vec.getY() * mult);
     }
 }
--- src/org/rangerrobotics/pathplanner/gui/MainScene.java
+++ src/org/rangerrobotics/pathplanner/gui/MainScene.java
@@ -29,14 +29,12 @@ public class MainScene {
     private static StackPane root;
     private static JFXTabPane layout;
     private static JFXSnackbar snackbar;
-    private static JFXTreeTableView<Waypoint> waypointTable = new JFXTreeTableView<>();
-    private static JFXTreeTableColumn<Waypoint, Double> xCol = new JFXTreeTableColumn<>("X Position");
-    private static JFXTreeTableColumn<Waypoint, Double> yCol = new JFXTreeTableColumn<>("Y Position");
-    private static JFXTreeTableColumn<Waypoint, Double> angleCol = new JFXTreeTableColumn<>("Angle");
-    private static JFXTreeTableColumn<Waypoint, Double> speedCol = new JFXTreeTableColumn<>("Speed");
-    private static ObservableList<Waypoint> waypoints;
+    private static JFXTreeTableView<Vector2> waypointTable = new JFXTreeTableView<>();
+    private static JFXTreeTableColumn<Vector2, Double> xCol = new JFXTreeTableColumn<>("X Position");
+    private static JFXTreeTableColumn<Vector2, Double> yCol = new JFXTreeTableColumn<>("Y Position");
+    private static ObservableList<Vector2> points;
     private static Canvas canvas;
-    private static ArrayList<Vector2> points = new ArrayList<>();
+    private static int pointDragIndex = -1;
 
     public static Scene getScene(){
         if(scene == null){
@@ -46,6 +44,7 @@ public class MainScene {
     }
 
     private static void createScene(){
+        points = FXCollections.observableArrayList();
         points.add(new Vector2(20, 20));
         points.add(new Vector2(170, 20));
         points.add(new Vector2(50, 200));
@@ -61,15 +60,35 @@ public class MainScene {
         BorderPane genTabLayout = new BorderPane();
         JFXButton test = new JFXButton("Beep");
         setupWaypointTable();
-        waypointTable.setMaxSize(302, 500);
+        waypointTable.setMaxSize(202, 500);
         VBox genLeft = new VBox(10);
         genLeft.getChildren().addAll(new Label("Henlo"), test, waypointTable);
         genTabLayout.setLeft(genLeft);
 
         canvas = new Canvas(400, 400);
+        canvas.setOnMousePressed(event -> {
+            System.out.println("Pressed X: " + event.getX() + ", Y: " + event.getY());
+            for(int i = 0; i < points.size(); i++){
+                System.out.println((Math.pow(event.getX() - points.get(i).getX() + 8, 2) + (Math.pow(event.getY() - points.get(i).getY() + 8, 2))));
+                if((Math.pow(event.getX() - points.get(i).getX(), 2) + (Math.pow(event.getY() - points.get(i).getY(), 2))) <= Math.pow(8, 2)){
+                    pointDragIndex = i;
+                    System.out.println(pointDragIndex);
+                }
+            }
+        });
+        canvas.setOnMouseReleased(event -> {
+            pointDragIndex = -1;
+        });
         canvas.setOnMouseDragged(event -> {
-            System.out.println("X: " + event.getX() + ", Y: " + event.getY());
+            System.out.println("Dragged X: " + event.getX() + ", Y: " + event.getY());
             //TODO: Move points on canvas, update curve
+            if(pointDragIndex != -1){
+                if(event.getX() >= 0 && event.getY() >= 0 && event.getX() <= canvas.getWidth() && event.getY() <= canvas.getHeight()) {
+                    points.get(pointDragIndex).setX(Math.round(event.getX()));
+                    points.get(pointDragIndex).setY(Math.round(event.getY()));
+                    updateCanvas();
+                }
+            }
         });
         drawCurve(canvas.getGraphicsContext2D());
         genTabLayout.setRight(canvas);
@@ -90,58 +109,52 @@ public class MainScene {
         for(double d = 0.01; d <= 1; d += 0.01){
             Vector2 p0 = Util.cubicCurve(points.get(0), points.get(1), points.get(2), points.get(3), d);
             Vector2 p1 = Util.cubicCurve(points.get(0), points.get(1), points.get(2), points.get(3), d + 0.01);
-            g.strokeLine(p0.x, p0.y, p1.x, p1.y);
+            g.strokeLine(p0.getX(), p0.getY(), p1.getX(), p1.getY());
         }
 
         g.setStroke(Color.color(0.1, 0.1, 0.1));
         g.setLineWidth(2);
-        g.strokeLine(points.get(0).x, points.get(0).y, points.get(1).x, points.get(1).y);
-        g.strokeLine(points.get(2).x, points.get(2).y, points.get(3).x, points.get(3).y);
+        g.strokeLine(points.get(0).getX(), points.get(0).getY(), points.get(1).getX(), points.get(1).getY());
+        g.strokeLine(points.get(2).getX(), points.get(2).getY(), points.get(3).getX(), points.get(3).getY());
 
         g.setFill(Color.RED);
         for(Vector2 p : points){
-            g.fillOval(p.x - 8, p.y - 8, 16, 16);
+            g.fillOval(p.getX() - 8, p.getY() - 8, 16, 16);
         }
     }
 
     private static void setupWaypointTable(){
-        xCol.setPrefWidth(75);
-        yCol.setPrefWidth(75);
-        angleCol.setPrefWidth(75);
-        speedCol.setPrefWidth(75);
+        xCol.setPrefWidth(100);
+        yCol.setPrefWidth(100);
         setupCellValueFactory(xCol, w -> w.x.asObject());
         setupCellValueFactory(yCol, w -> w.y.asObject());
-        setupCellValueFactory(angleCol, w -> w.angle.asObject());
-        setupCellValueFactory(speedCol, w -> w.speed.asObject());
 
         //add editors
-        xCol.setCellFactory((TreeTableColumn<Waypoint, Double> param) -> new GenericEditableTreeTableCell<>(new DoubleTextFieldEditorBuilder()));
-        xCol.setOnEditCommit((TreeTableColumn.CellEditEvent<Waypoint, Double> t) -> {
+        xCol.setCellFactory((TreeTableColumn<Vector2, Double> param) -> new GenericEditableTreeTableCell<>(new DoubleTextFieldEditorBuilder()));
+        xCol.setOnEditCommit((TreeTableColumn.CellEditEvent<Vector2, Double> t) -> {
             t.getTreeTableView().getTreeItem(t.getTreeTablePosition().getRow()).getValue().x.set(t.getNewValue());
-            canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-            drawCurve(canvas.getGraphicsContext2D());
+            updateCanvas();
         });
-        yCol.setCellFactory((TreeTableColumn<Waypoint, Double> param) -> new GenericEditableTreeTableCell<>(new DoubleTextFieldEditorBuilder()));
-        yCol.setOnEditCommit((TreeTableColumn.CellEditEvent<Waypoint, Double> t) -> t.getTreeTableView().getTreeItem(t.getTreeTablePosition().getRow()).getValue().y.set(t.getNewValue()));
-        angleCol.setCellFactory((TreeTableColumn<Waypoint, Double> param) -> new GenericEditableTreeTableCell<>(new DoubleTextFieldEditorBuilder()));
-        angleCol.setOnEditCommit((TreeTableColumn.CellEditEvent<Waypoint, Double> t) -> t.getTreeTableView().getTreeItem(t.getTreeTablePosition().getRow()).getValue().angle.set(t.getNewValue()));
-        speedCol.setCellFactory((TreeTableColumn<Waypoint, Double> param) -> new GenericEditableTreeTableCell<>(new DoubleTextFieldEditorBuilder()));
-        speedCol.setOnEditCommit((TreeTableColumn.CellEditEvent<Waypoint, Double> t) -> t.getTreeTableView().getTreeItem(t.getTreeTablePosition().getRow()).getValue().speed.set(t.getNewValue()));
+        yCol.setCellFactory((TreeTableColumn<Vector2, Double> param) -> new GenericEditableTreeTableCell<>(new DoubleTextFieldEditorBuilder()));
+        yCol.setOnEditCommit((TreeTableColumn.CellEditEvent<Vector2, Double> t) -> {
+            t.getTreeTableView().getTreeItem(t.getTreeTablePosition().getRow()).getValue().y.set(t.getNewValue());
+            updateCanvas();
+        });
 
-        waypoints = FXCollections.observableArrayList();
-        waypoints.add(new Waypoint(0, 10, 0, 0));
-        waypoints.add(new Waypoint(5, 5, 0, 5));
-        waypoints.add(new Waypoint(10, 10, 0, 0));
-        final TreeItem<Waypoint> root = new RecursiveTreeItem<>(waypoints, RecursiveTreeObject::getChildren);
+        final TreeItem<Vector2> root = new RecursiveTreeItem<>(points, RecursiveTreeObject::getChildren);
         waypointTable.setRoot(root);
         waypointTable.setShowRoot(false);
         waypointTable.setEditable(true);
-        waypointTable.getColumns().setAll(xCol, yCol, angleCol, speedCol);
-//        waypointTableCount.textProperty().bind(Bindings.createStringBinding(() -> "( " + waypointTable.getCurrentItemsCount() + waypointTable.currentItemsCountProperty() + " )"));
+        waypointTable.getColumns().setAll(xCol, yCol);
     }
 
-    private static <T> void setupCellValueFactory(JFXTreeTableColumn<Waypoint, T> column, Function<Waypoint, ObservableValue<T>> mapper){
-        column.setCellValueFactory((TreeTableColumn.CellDataFeatures<Waypoint, T> param) -> {
+    private static void updateCanvas(){
+        canvas.getGraphicsContext2D().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawCurve(canvas.getGraphicsContext2D());
+    }
+
+    private static <T> void setupCellValueFactory(JFXTreeTableColumn<Vector2, T> column, Function<Vector2, ObservableValue<T>> mapper){
+        column.setCellValueFactory((TreeTableColumn.CellDataFeatures<Vector2, T> param) -> {
             if(column.validateValue(param)){
                 return mapper.apply(param.getValue().getValue());
             }else{

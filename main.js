0d7e60629db09b449ce3165de00bfaba9cd6a86a
Michael Jansen <mjansen4857@gmail.com>
2018-12-13 16:44:57 -0500

    Update main.js
    document + comment main.js

--- main.js
+++ main.js
@@ -24,15 +24,28 @@ const sftp = new Client();
 
 let win;
 
+/**
+ * Track an event in google analytics
+ * @param category the category of the event
+ * @param action the event action
+ * @param label the event label
+ * @param value the event value
+ */
 function trackEvent(category, action, label, value){
 	usr.event(category, action, label, value).send();
 }
 global.trackEvent = trackEvent;
 
+/**
+ * Track a screen view. Currently used to track when the app is launched
+ */
 function trackScreen(){
 	usr.screenview({cd: 'PathPlanner', an: 'pathplanner', av:app.getVersion()}).send();
 }
 
+/**
+ * Create the main window
+ */
 function createWindow(){
 	win = new BrowserWindow({width: 1200, height: 745, icon: 'build/icon.png', frame: false, resizable: false});
 	win.setMenu(null);
@@ -49,6 +62,7 @@ function createWindow(){
 	}
 }
 
+// When the app is ready, create the window and check for updates if on windows
 app.on('ready', function(){
 	createWindow();
 	if(os.platform() == 'win32'){
@@ -56,6 +70,7 @@ app.on('ready', function(){
 	}
 });
 
+// Quit the app when all windows are closed
 app.on('window-all-closed', () => {
 	app.quit();
 });
@@ -66,19 +81,23 @@ app.on('activate', () => {
 	}
 });
 
+// Notify the renderer when downloading an update
 autoUpdater.on('update-available', (info) => {
 	win.webContents.send('downloading-update', info.version);
 });
 
+// Notify the renderer that an update is ready
 autoUpdater.on('update-downloaded', (info) => {
 	win.webContents.send('update-ready');
 });
 
+// Update the app when the user clicks the restart button
 ipc.on('quit-and-install', (event, data) => {
 	autoUpdater.quitAndInstall();
 	trackEvent('User Interaction', 'Install Update')
 });
 
+// Create a hidden window to generate the path to avoid delaying the main window
 ipc.on('generate', function(event, data){
 	log.info('Starting generation worker...')
 	var worker = new BrowserWindow({show: false});
@@ -86,9 +105,11 @@ ipc.on('generate', function(event, data){
 	worker.on('ready-to-show', () => worker.webContents.send('generate-path', data));
 });
 
+// Upload the generated path files to a robot using sftp
 ipc.on('deploy-segments', function (event, data) {
 	log.info('Connecting to robot...');
 	win.webContents.send('connecting');
+	// Connect to the robot
 	sftp.connect({
 		host: 'roborio-' + data.team + '-frc.local',
 		username: 'lvuser',
@@ -96,6 +117,7 @@ ipc.on('deploy-segments', function (event, data) {
 	}).then(() => {
 		log.info('Uploading files...');
 		win.webContents.send('uploading');
+		// Helper function to upload both path files
 		let upload = function(){
 			sftp.put(Buffer.from(data.left), data.path + '/' + data.name + '_left.csv').then((response) => {
 				log.info(response);
@@ -107,38 +129,46 @@ ipc.on('deploy-segments', function (event, data) {
 				});
 			});
 		};
+		// Make the destination folder and upload files if it doesn't exist, otherwise just upload files
 		sftp.mkdir(data.path, true).then(() => {
 			upload();
 		}).catch(() => {
 			upload();
 		});
 	}).catch((err) => {
+		// Failed to connect to the robot
 		log.error(err);
 		win.webContents.send('connect-failed');
 		sftp.end();
 	});
 });
 
+// Allow the renderer to get the app version
 ipc.on('request-version', function(event, data){
 	win.webContents.send('app-version', app.getVersion());
 });
 
+// Update the last directory where path files were saved
 ipc.on('update-last-generate-dir', function(event, data){
 	win.webContents.send('update-last-generate-dir', data);
 });
 
+// Notify the renderer that path files were saved
 ipc.on('files-saved', function(event, data){
 	win.webContents.send('files-saved', data);
 });
 
+// Notify the renderer that path arrays were copied
 ipc.on('copied-to-clipboard', function(event, data){
 	win.webContents.send('copied-to-clipboard', data);
 });
 
+// Send the generated path to the renderer for the preview
 ipc.on('preview-segments', function(event, data){
 	win.webContents.send('preview-segments', data);
-})
+});
 
+// Notify the renderer that the generator is running
 ipc.on('generating', function(event, data){
 	win.webContents.send('generating', data);
 });

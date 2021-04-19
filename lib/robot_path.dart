431cb5998183be758ca46ed37fd17fbbc2492568
Michael Jansen <mjansen4857@gmail.com>
2021-04-18 21:05:53 -0400

    draggable path points

--- lib/robot_path.dart
+++ lib/robot_path.dart
@@ -11,6 +11,10 @@ class Waypoint {
   Point prevControl;
   Point nextControl;
 
+  bool _isAnchorDragging = false;
+  bool _isNextControlDragging = false;
+  bool _isPrevControlDragging = false;
+
   Waypoint({this.anchorPoint, this.prevControl, this.nextControl});
 
   bool isStartPoint() {
@@ -19,5 +23,94 @@ class Waypoint {
 
   bool isEndPoint() {
     return nextControl == null;
+  }
+
+  double getAngleRadians() {
+    if (isStartPoint()) {
+      return -atan2(
+          nextControl.y - anchorPoint.y, nextControl.x - anchorPoint.x);
+    } else {
+      return -atan2(
+          anchorPoint.y - prevControl.y, anchorPoint.x - prevControl.x);
+    }
+  }
+
+  bool isPointInAnchor(double xPos, double yPos, double radius) {
+    return pow(xPos - anchorPoint.x, 2) + pow(yPos - anchorPoint.y, 2) <
+        pow(radius, 2);
+  }
+
+  bool isPointInNextControl(double xPos, double yPos, double radius) {
+    if (nextControl != null) {
+      return pow(xPos - nextControl.x, 2) + pow(yPos - nextControl.y, 2) <
+          pow(radius, 2);
+    }
+    return false;
+  }
+
+  bool isPointInPrevControl(double xPos, double yPos, double radius) {
+    if (prevControl != null) {
+      return pow(xPos - prevControl.x, 2) + pow(yPos - prevControl.y, 2) <
+          pow(radius, 2);
+    }
+    return false;
+  }
+
+  bool startDragging(
+      double xPos, double yPos, double anchorRadius, double controlRadius) {
+    if (isPointInAnchor(xPos, yPos, anchorRadius)) {
+      return _isAnchorDragging = true;
+    } else if (isPointInNextControl(xPos, yPos, controlRadius)) {
+      return _isNextControlDragging = true;
+    } else if (isPointInPrevControl(xPos, yPos, controlRadius)) {
+      return _isPrevControlDragging = true;
+    }
+    return false;
+  }
+
+  void dragUpdate(double dx, double dy) {
+    if (_isAnchorDragging) {
+      anchorPoint = Point(anchorPoint.x + dx, anchorPoint.y + dy);
+      if (nextControl != null) {
+        nextControl = Point(nextControl.x + dx, nextControl.y + dy);
+      }
+      if (prevControl != null) {
+        prevControl = Point(prevControl.x + dx, prevControl.y + dy);
+      }
+    } else if (_isNextControlDragging) {
+      nextControl = Point(nextControl.x + dx, nextControl.y + dy);
+
+      if (prevControl != null) {
+        var dst = anchorPoint.distanceTo(prevControl);
+        var dir =
+            Point(anchorPoint.x - nextControl.x, anchorPoint.y - nextControl.y);
+        var mag = dir.distanceTo(Point(0, 0));
+        dir = Point(dir.x / mag, dir.y / mag);
+
+        var control = Point(dir.x * dst, dir.y * dst);
+        prevControl =
+            Point(anchorPoint.x + control.x, anchorPoint.y + control.y);
+      }
+    } else if (_isPrevControlDragging) {
+      prevControl = Point(prevControl.x + dx, prevControl.y + dy);
+
+      if (nextControl != null) {
+        var dst = anchorPoint.distanceTo(nextControl);
+        var dir =
+            Point(anchorPoint.x - prevControl.x, anchorPoint.y - prevControl.y);
+        var mag = dir.distanceTo(Point(0, 0));
+        dir = Point(dir.x / mag, dir.y / mag);
+
+        var control = Point(dir.x * dst, dir.y * dst);
+        nextControl =
+            Point(anchorPoint.x + control.x, anchorPoint.y + control.y);
+      }
+    }
+  }
+
+  void stopDragging() {
+    _isPrevControlDragging = false;
+    _isNextControlDragging = false;
+    _isAnchorDragging = false;
   }
 }
--- lib/widgets/path_editor/path_editor.dart
+++ lib/widgets/path_editor/path_editor.dart
@@ -14,42 +14,88 @@ class PathEditor extends StatefulWidget {
 }
 
 class _PathEditorState extends State<PathEditor> {
+  Waypoint _draggedPoint;
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        child: Stack(
-          children: [
-            Image(image: AssetImage('images/field20.png')),
-            Positioned.fill(
-              child: Container(
-                constraints: BoxConstraints(maxWidth: 1200, maxHeight: 600),
-                child: CustomPaint(
-                  painter: PathPainter(widget.path),
+      child: GestureDetector(
+        onPanStart: (details) {
+          for (Waypoint w in widget.path.waypoints.reversed) {
+            if (w.startDragging(
+                xPixelsToMeters(details.localPosition.dx),
+                yPixelsToMeters(details.localPosition.dy),
+                pixelsToMeters(8),
+                pixelsToMeters(6))) {
+              _draggedPoint = w;
+              break;
+            }
+          }
+        },
+        onPanUpdate: (details) {
+          if (_draggedPoint != null) {
+            setState(() {
+              _draggedPoint.dragUpdate(pixelsToMeters(details.delta.dx),
+                  pixelsToMeters(-details.delta.dy));
+            });
+          }
+        },
+        onPanEnd: (details) {
+          if (_draggedPoint != null) {
+            _draggedPoint.stopDragging();
+            _draggedPoint = null;
+          }
+        },
+        child: Container(
+          child: Stack(
+            children: [
+              Image(image: AssetImage('images/field20.png')),
+              Positioned.fill(
+                child: Container(
+                  constraints: BoxConstraints(maxWidth: 1200, maxHeight: 600),
+                  child: CustomPaint(
+                    painter: PathPainter(widget.path),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  double xPixelsToMeters(double pixels) {
+    return (pixels - 76) / 66.11 / PathPainter.scale;
+  }
+
+  double yPixelsToMeters(double pixels) {
+    return (600 - pixels - 78) / 66.11 / PathPainter.scale;
+  }
+
+  double pixelsToMeters(double pixels) {
+    return pixels / 66.11 / PathPainter.scale;
   }
 }
 
 class PathPainter extends CustomPainter {
   var defaultSize = Size(1200, 600);
+  var robotSize = Size(0.75, 0.75);
+  static double scale = 1;
   RobotPath path;
 
   PathPainter(this.path);
 
   @override
   void paint(Canvas canvas, Size size) {
-    var scale = size.width / defaultSize.width;
+    scale = size.width / defaultSize.width;
 
     paintCenterPath(canvas, scale);
+    // paintDualPaths(canvas, scale);
 
-    for (int i = 0; i < path.waypoints.length; i++) {
-      paintWaypoint(canvas, scale, path.waypoints[i]);
+    for (Waypoint w in path.waypoints) {
+      paintRobotOutline(canvas, scale, w);
+      paintWaypoint(canvas, scale, w);
     }
   }
 
@@ -70,6 +116,79 @@ class PathPainter extends CustomPainter {
 
       canvas.drawPath(p, paint);
     }
+  }
+
+  // no worky. would have to make custom bezier drawing
+  void paintDualPaths(Canvas canvas, double scale) {
+    var paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.grey[300]
+      ..strokeWidth = 2;
+
+    for (int i = 0; i < path.waypoints.length - 1; i++) {
+      Path p = Path();
+
+      double angle1 = path.waypoints[i].getAngleRadians();
+      double angle2 = path.waypoints[i + 1].getAngleRadians();
+      double halfWidth = metersToPixels(robotSize.width / 2, scale);
+
+      Offset p0L = pointToPixelOffset(path.waypoints[i].anchorPoint, scale)
+          .translate(halfWidth * sin(angle1), -(halfWidth * cos(angle1)));
+      Offset p1L = pointToPixelOffset(path.waypoints[i].nextControl, scale)
+          .translate(halfWidth * sin(angle1), -(halfWidth * cos(angle1)));
+      Offset p2L = pointToPixelOffset(path.waypoints[i + 1].prevControl, scale)
+          .translate(halfWidth * sin(angle2), -(halfWidth * cos(angle2)));
+      Offset p3L = pointToPixelOffset(path.waypoints[i + 1].anchorPoint, scale)
+          .translate(halfWidth * sin(angle2), -(halfWidth * cos(angle2)));
+
+      Offset p0R = pointToPixelOffset(path.waypoints[i].anchorPoint, scale)
+          .translate(-(halfWidth * sin(angle1)), halfWidth * cos(angle1));
+      Offset p1R = pointToPixelOffset(path.waypoints[i].nextControl, scale)
+          .translate(-(halfWidth * sin(angle1)), halfWidth * cos(angle1));
+      Offset p2R = pointToPixelOffset(path.waypoints[i + 1].prevControl, scale)
+          .translate(-(halfWidth * sin(angle2)), halfWidth * cos(angle2));
+      Offset p3R = pointToPixelOffset(path.waypoints[i + 1].anchorPoint, scale)
+          .translate(-(halfWidth * sin(angle2)), halfWidth * cos(angle2));
+
+      p.moveTo(p0L.dx, p0L.dy);
+      p.cubicTo(p1L.dx, p1L.dy, p2L.dx, p2L.dy, p3L.dx, p3L.dy);
+
+      p.moveTo(p0R.dx, p0R.dy);
+      p.cubicTo(p1R.dx, p1R.dy, p2R.dx, p2R.dy, p3R.dx, p3R.dy);
+
+      canvas.drawPath(p, paint);
+    }
+  }
+
+  void paintRobotOutline(Canvas canvas, double scale, Waypoint waypoint) {
+    var paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.grey[400]
+      ..strokeWidth = 2;
+
+    Offset center = pointToPixelOffset(waypoint.anchorPoint, scale);
+    double angle = waypoint.getAngleRadians();
+    double halfWidth = metersToPixels(robotSize.width / 2, scale);
+    double halfLength = metersToPixels(robotSize.height / 2, scale);
+
+    Offset l = Offset(center.dx + (halfWidth * sin(angle)),
+        center.dy - (halfWidth * cos(angle)));
+    Offset r = Offset(center.dx - (halfWidth * sin(angle)),
+        center.dy + (halfWidth * cos(angle)));
+
+    Offset frontLeft = Offset(
+        l.dx + (halfLength * cos(angle)), l.dy + (halfLength * sin(angle)));
+    Offset backLeft = Offset(
+        l.dx - (halfLength * cos(angle)), l.dy - (halfLength * sin(angle)));
+    Offset frontRight = Offset(
+        r.dx + (halfLength * cos(angle)), r.dy + (halfLength * sin(angle)));
+    Offset backRight = Offset(
+        r.dx - (halfLength * cos(angle)), r.dy - (halfLength * sin(angle)));
+
+    canvas.drawLine(backLeft, frontLeft, paint);
+    canvas.drawLine(frontLeft, frontRight, paint);
+    canvas.drawLine(frontRight, backRight, paint);
+    canvas.drawLine(backRight, backLeft, paint);
   }
 
   void paintWaypoint(Canvas canvas, double scale, Waypoint waypoint) {
@@ -134,6 +253,10 @@ class PathPainter extends CustomPainter {
     return Offset((point.x * 66.11) + 76,
             defaultSize.height - ((point.y * 66.11) + 78))
         .scale(scale, scale);
+  }
+
+  double metersToPixels(double meters, double scale) {
+    return meters * 66.11 * scale;
   }
 
   @override

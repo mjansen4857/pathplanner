e49a3d31e29ce87c9c3f5b9700edfd643803d35f
Michael Jansen <mjansen4857@gmail.com>
2021-04-27 15:18:57 -0400

    add action button to deploy robot code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,6 +17,7 @@ import 'package:pathplanner/widgets/drawer_tiles/settings_tile.dart';
 import 'package:pathplanner/widgets/keyboard_shortcuts/keyboard_shortcuts.dart';
 import 'package:pathplanner/widgets/path_editor/path_editor.dart';
 import 'package:pathplanner/widgets/window_button/window_button.dart';
+import 'package:process_run/shell.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -98,6 +99,39 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           _buildBody(context),
           _buildUpdateNotification(),
         ],
+      ),
+      floatingActionButton: Visibility(
+        visible: _currentProject != null,
+        child: Tooltip(
+          message: 'Deploy Robot Code',
+          waitDuration: Duration(milliseconds: 500),
+          child: FloatingActionButton(
+            child: Icon(Icons.send_rounded),
+            backgroundColor: Colors.grey[900],
+            foregroundColor: Colors.green,
+            onPressed: () async {
+              Shell shell = Shell().cd(_currentProject!.path);
+              _showSnackbar(context, 'Deploying robot code...',
+                  duration: Duration(minutes: 5));
+              try {
+                ProcessResult result =
+                    await shell.runExecutableArguments('gradlew', ['deploy']);
+                ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                if (result.exitCode == 0) {
+                  _showSnackbar(context, 'Successfully deployed.',
+                      textColor: Colors.green);
+                } else {
+                  _showSnackbar(context, 'Failed to deploy.',
+                      textColor: Colors.red);
+                }
+              } on ShellException catch (e) {
+                ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                _showSnackbar(context, 'Failed to deploy.',
+                    textColor: Colors.red);
+              }
+            },
+          ),
+        ),
       ),
     );
   }
@@ -620,5 +654,17 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         );
       }
     }
+  }
+
+  void _showSnackbar(BuildContext context, String message,
+      {Duration? duration, Color textColor = Colors.white}) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: textColor, fontSize: 16),
+      ),
+      duration: duration ?? Duration(milliseconds: 4000),
+      backgroundColor: Colors.grey[900],
+    ));
   }
 }

5eda9346642a764990e0252c0277b683887f98e5
Michael Jansen <mjansen4857@gmail.com>
2021-11-01 22:31:49 -0400

    fix c++ lib compilation problem

--- pathplannerlib/src/main/native/cpp/pathplanner/lib/PathPlanner.cpp
+++ pathplannerlib/src/main/native/cpp/pathplanner/lib/PathPlanner.cpp
@@ -1,38 +1,34 @@
-//#include "pathplanner/lib/PathPlanner.h"
-//#include <frc/geometry/Translation2d.h>
-//#include <frc/Filesystem.h>
-//#include <wpi/SmallString.h>
-//#include <wpi/Path.h>
-//#include <wpi/raw_istream.h>
-//#include <wpi/json.h>
-//#include <vector>
-//#include <iostream>
-//
-//using namespace pathplanner;
-//
-//double PathPlanner::resolution = 0.004;
-//
-//Path PathPlanner::loadPath(std::string name, units::meters_per_second_t maxVel, units::meters_per_second_squared_t maxAccel, bool reversed){
-//    std::string line;
-//    wpi::SmallString<128> filePath;
-//
-//    frc::filesystem::GetDeployDirectory(filePath);
-//    wpi::sys::path::append(filePath, "pathplanner");
-//    wpi::sys::path::append(filePath, name + ".path");
-//
-//    std::error_code error_code;
-//    wpi::raw_fd_istream input{filePath.str(), error_code};
-//
-//    if(error_code){
-//        throw std::runtime_error(("Cannot open file: " + filePath).str());
-//    }
-//
-//    wpi::json json;
-//    input >> json;
-//
-//    std::cout << json.is_array() << "\n";
-//
-//    std::vector<Path::State> test;
-//
-//    return Path(test);
-//}
+#include "pathplanner/lib/PathPlanner.h"
+#include <frc/geometry/Translation2d.h>
+#include <frc/Filesystem.h>
+#include <wpi/SmallString.h>
+#include <wpi/raw_istream.h>
+#include <wpi/json.h>
+#include <vector>
+#include <iostream>
+
+using namespace pathplanner;
+
+double PathPlanner::resolution = 0.004;
+
+Path PathPlanner::loadPath(std::string name, units::meters_per_second_t maxVel, units::meters_per_second_squared_t maxAccel, bool reversed){
+   std::string line;
+
+   std::string filePath = frc::filesystem::GetDeployDirectory() + "/pathplanner/" + name + ".path";
+
+   std::error_code error_code;
+   wpi::raw_fd_istream input{filePath, error_code};
+
+   if(error_code){
+       throw std::runtime_error(("Cannot open file: " + filePath));
+   }
+
+   wpi::json json;
+   input >> json;
+
+   std::cout << "is array: " << json.is_array() << std::endl;
+
+   std::vector<Path::State> test;
+
+   return Path(test);
+}

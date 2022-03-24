5d75ad5e1a0fdf5026b1f277e687eda066446590
Michael Jansen <mjansen4857@gmail.com>
2022-03-24 14:20:29 -0400

    add some basic unit tests for generation

---
+++ test/services/generator/trajectory_test.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pathplanner/robot_path/robot_path.dart';
+import 'package:pathplanner/robot_path/waypoint.dart';
+import 'package:pathplanner/services/generator/trajectory.dart';
+
+void main() {
+  group("Generate trajectory", () {
+    // Trajectory is a pretty complex class, so just check that the runtime of
+    // the path is correct to make sure something isn't very wrong
+    test('simple path', () async {
+      List<Waypoint> waypoints = [
+        Waypoint(
+          anchorPoint: Point(1.0, 3.0),
+          nextControl: Point(2.0, 3.0),
+        ),
+        Waypoint(
+          prevControl: Point(3.0, 4.0),
+          anchorPoint: Point(3.0, 5.0),
+        ),
+      ];
+      RobotPath path = RobotPath(waypoints, maxVelocity: 8, maxAcceleration: 5);
+
+      Trajectory traj = await Trajectory.generateFullTrajectory(path);
+
+      expect(traj.getRuntime(), closeTo(1.585, 0.001));
+    });
+
+    test('complex path', () async {
+      List<Waypoint> waypoints = [
+        Waypoint(
+          anchorPoint: Point(1.0, 3.0),
+          nextControl: Point(2.0, 3.0),
+        ),
+        Waypoint(
+          prevControl: Point(3.0, 4.0),
+          anchorPoint: Point(3.0, 5.0),
+          isReversal: true,
+        ),
+        Waypoint(
+          prevControl: Point(4.0, 3.0),
+          anchorPoint: Point(5.0, 3.0),
+        ),
+      ];
+      RobotPath path = RobotPath(waypoints, maxVelocity: 8, maxAcceleration: 5);
+
+      Trajectory traj = await Trajectory.generateFullTrajectory(path);
+
+      expect(traj.getRuntime(), closeTo(3.171, 0.001));
+    });
+  });
+}

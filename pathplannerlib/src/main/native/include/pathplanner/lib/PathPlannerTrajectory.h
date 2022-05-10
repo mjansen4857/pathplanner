9911935172985c753f4712018f3fa7f748313d38
Michael Jansen <mjansen4857@gmail.com>
2022-05-10 13:28:01 -0400

    use friend classes so private data can remain private

--- pathplannerlib/src/main/native/include/pathplanner/lib/PathPlannerTrajectory.h
+++ pathplannerlib/src/main/native/include/pathplanner/lib/PathPlannerTrajectory.h
@@ -21,19 +21,25 @@ namespace pathplanner{
             class PathPlannerState{
                 public:
                     units::second_t time = 0_s;
-                    units::meter_t position = 0_m;
                     units::meters_per_second_t velocity = 0_mps;
                     units::meters_per_second_squared_t acceleration = 0_mps_sq;
                     frc::Pose2d pose;
                     units::curvature_t curvature{0.0};
                     units::radians_per_second_t angularVel;
                     units::radians_per_second_squared_t angularAccel;
-                    frc::Rotation2d holonomicRotation;
-                    PathPlannerState interpolate(PathPlannerState endVal, double t);
+                    frc::Rotation2d holonomicRotation;     
+            
+                private:
+                    units::meter_t position = 0_m;
                     units::meter_t curveRadius = 0_m;
                     units::meter_t deltaPos = 0_m;
-            };
 
+                    PathPlannerState interpolate(PathPlannerState endVal, double t);   
+
+                    friend class PathPlannerTrajectory;
+            };
+        
+        private:
             class Waypoint{
                 public:
                     frc::Translation2d anchorPoint;
@@ -52,14 +58,16 @@ namespace pathplanner{
                         this->isReversal = isReversal;
                     }
             };
-        
-        private:
+
             std::vector<PathPlannerState> states;
+
             std::vector<PathPlannerState> joinSplines(std::vector<Waypoint> pathPoints, units::meters_per_second_t maxVel, double step);
             void calculateMaxVel(std::vector<PathPlannerState>& states, units::meters_per_second_t maxVel, units::meters_per_second_squared_t maxAccel, bool reversed);
             void calculateVelocity(std::vector<PathPlannerState>& states, std::vector<Waypoint> pathPoints, units::meters_per_second_squared_t maxAccel);
             void recalculateValues(std::vector<PathPlannerState>& states, bool reversed);
             units::meter_t calculateRadius(PathPlannerState s0, PathPlannerState s1, PathPlannerState s2);
+
+            friend class PathPlanner;
         
         public:
             PathPlannerTrajectory(std::vector<Waypoint> waypoints, units::meters_per_second_t maxVelocity, units::meters_per_second_squared_t maxAcceleration, bool reversed);

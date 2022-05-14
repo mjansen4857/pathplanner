03d93cde9e6578e52fa72a03d94130f8a62baf08
Michael Jansen <mjansen4857@gmail.com>
2022-05-13 22:45:07 -0400

    fix padding in preview editor

--- lib/widgets/path_editor/editors/preview_editor.dart
+++ lib/widgets/path_editor/editors/preview_editor.dart
@@ -63,7 +63,7 @@ class _PreviewEditorState extends State<PreviewEditor>
           child: InteractiveViewer(
             child: Container(
               child: Padding(
-                padding: const EdgeInsets.all(48.0),
+                padding: const EdgeInsets.fromLTRB(48, 24, 48, 48),
                 child: Stack(
                   children: [
                     widget.fieldImage,

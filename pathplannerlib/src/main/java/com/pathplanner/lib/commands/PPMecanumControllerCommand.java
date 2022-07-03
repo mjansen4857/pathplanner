367b3847f865777a7c8ab521916c379f57eb4597
Michael Jansen <mjansen4857@gmail.com>
2022-07-03 11:44:18 -0400

    update java swerve and mecanum commands to use PPHolonomicDriveController

--- pathplannerlib/src/main/java/com/pathplanner/lib/commands/PPMecanumControllerCommand.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/commands/PPMecanumControllerCommand.java
@@ -6,10 +6,10 @@ package com.pathplanner.lib.commands;
 
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
-import edu.wpi.first.math.controller.HolonomicDriveController;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
 import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.Timer;
@@ -19,18 +19,16 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
- * A command that uses two PID controllers ({@link PIDController}) and a ProfiledPIDController
- * ({@link ProfiledPIDController}) to follow a trajectory {@link PathPlannerTrajectory} with a
- * mecanum drive.
+ * Custom PathPlanner version of MecanumControllerCommand
  */
 public class PPMecanumControllerCommand extends CommandBase {
-  private final Timer m_timer = new Timer();
-  private final PathPlannerTrajectory m_trajectory;
-  private final Supplier<Pose2d> m_pose;
-  private final MecanumDriveKinematics m_kinematics;
-  private final HolonomicDriveController m_controller;
-  private final double m_maxWheelVelocityMetersPerSecond;
-  private final Consumer<MecanumDriveWheelSpeeds> m_outputWheelSpeeds;
+  private final Timer timer = new Timer();
+  private final PathPlannerTrajectory trajectory;
+  private final Supplier<Pose2d> poseSupplier;
+  private final MecanumDriveKinematics kinematics;
+  private final PPHolonomicDriveController controller;
+  private final double maxWheelVelocityMetersPerSecond;
+  private final Consumer<MecanumDriveWheelSpeeds> outputWheelSpeeds;
 
   /**
    * Constructs a new PPMecanumControllerCommand that when executed will follow the provided
@@ -40,67 +38,90 @@ public class PPMecanumControllerCommand extends CommandBase {
    * this is left to the user, since it is not appropriate for paths with non-stationary end-states.
    *
    * @param trajectory The Pathplanner trajectory to follow.
-   * @param pose A function that supplies the robot pose - use one of the odometry classes to
+   * @param poseSupplier A function that supplies the robot pose - use one of the odometry classes to
    *     provide this.
    * @param kinematics The kinematics for the robot drivetrain.
    * @param xController The Trajectory Tracker PID controller for the robot's x position.
    * @param yController The Trajectory Tracker PID controller for the robot's y position.
-   * @param thetaController The Trajectory Tracker PID controller for angle for the robot.
+   * @param rotationController The Trajectory Tracker PID controller for angle for the robot.
    * @param maxWheelVelocityMetersPerSecond The maximum velocity of a drivetrain wheel.
    * @param outputWheelSpeeds A MecanumDriveWheelSpeeds object containing the output wheel speeds.
    * @param requirements The subsystems to require.
    */
   public PPMecanumControllerCommand(
       PathPlannerTrajectory trajectory,
-      Supplier<Pose2d> pose,
+      Supplier<Pose2d> poseSupplier,
       MecanumDriveKinematics kinematics,
       PIDController xController,
       PIDController yController,
-      ProfiledPIDController thetaController,
+      PIDController rotationController,
       double maxWheelVelocityMetersPerSecond,
       Consumer<MecanumDriveWheelSpeeds> outputWheelSpeeds,
       Subsystem... requirements) {
-    m_trajectory = trajectory;
-    m_pose = pose;
-    m_kinematics = kinematics;
-
-    m_controller = new HolonomicDriveController(xController, yController, thetaController);
-
-    m_maxWheelVelocityMetersPerSecond = maxWheelVelocityMetersPerSecond;
-
-    m_outputWheelSpeeds = outputWheelSpeeds;
+    this.trajectory = trajectory;
+    this.poseSupplier = poseSupplier;
+    this.kinematics = kinematics;
+    this.controller = new PPHolonomicDriveController(xController, yController, rotationController);
+    this.maxWheelVelocityMetersPerSecond = maxWheelVelocityMetersPerSecond;
+    this.outputWheelSpeeds = outputWheelSpeeds;
 
     addRequirements(requirements);
+  }
+  /**
+   * Constructs a new PPMecanumControllerCommand that when executed will follow the provided
+   * trajectory. The user should implement a velocity PID on the desired output wheel velocities.
+   *
+   * <p>Note: The controllers will *not* set the outputVolts to zero upon completion of the path -
+   * this is left to the user, since it is not appropriate for paths with non-stationary end-states.
+   *
+   * @param trajectory The Pathplanner trajectory to follow.
+   * @param poseSupplier A function that supplies the robot pose - use one of the odometry classes to
+   *     provide this.
+   * @param kinematics The kinematics for the robot drivetrain.
+   * @param xyController The Trajectory Tracker PID controller for the robot's x and y position.
+   * @param rotationController The Trajectory Tracker PID controller for angle for the robot.
+   * @param maxWheelVelocityMetersPerSecond The maximum velocity of a drivetrain wheel.
+   * @param outputWheelSpeeds A MecanumDriveWheelSpeeds object containing the output wheel speeds.
+   * @param requirements The subsystems to require.
+   */
+  public PPMecanumControllerCommand(
+          PathPlannerTrajectory trajectory,
+          Supplier<Pose2d> poseSupplier,
+          MecanumDriveKinematics kinematics,
+          PIDController xyController,
+          PIDController rotationController,
+          double maxWheelVelocityMetersPerSecond,
+          Consumer<MecanumDriveWheelSpeeds> outputWheelSpeeds,
+          Subsystem... requirements) {
+    this(trajectory, poseSupplier, kinematics, xyController, xyController, rotationController, maxWheelVelocityMetersPerSecond, outputWheelSpeeds, requirements);
   }
 
   @Override
   public void initialize() {
-    m_timer.reset();
-    m_timer.start();
+    this.timer.reset();
+    this.timer.start();
   }
 
   @Override
-  @SuppressWarnings("LocalVariableName")
   public void execute() {
-    double curTime = m_timer.get();
-    var desiredState = (PathPlannerState) m_trajectory.sample(curTime);
+    double currentTime = this.timer.get();
+    PathPlannerState desiredState = (PathPlannerState) this.trajectory.sample(currentTime);
 
-    var targetChassisSpeeds =
-        m_controller.calculate(m_pose.get(), desiredState, desiredState.holonomicRotation);
-    var targetWheelSpeeds = m_kinematics.toWheelSpeeds(targetChassisSpeeds);
+    ChassisSpeeds targetChassisSpeeds = this.controller.calculate(this.poseSupplier.get(), desiredState);
+    MecanumDriveWheelSpeeds targetWheelSpeeds = this.kinematics.toWheelSpeeds(targetChassisSpeeds);
 
-    targetWheelSpeeds.desaturate(m_maxWheelVelocityMetersPerSecond);
+    targetWheelSpeeds.desaturate(this.maxWheelVelocityMetersPerSecond);
 
-    m_outputWheelSpeeds.accept(targetWheelSpeeds);
+    this.outputWheelSpeeds.accept(targetWheelSpeeds);
   }
 
   @Override
   public void end(boolean interrupted) {
-    m_timer.stop();
+    this.timer.stop();
   }
 
   @Override
   public boolean isFinished() {
-    return m_timer.hasElapsed(m_trajectory.getTotalTimeSeconds());
+    return this.timer.hasElapsed(this.trajectory.getTotalTimeSeconds());
   }
 }
--- pathplannerlib/src/main/java/com/pathplanner/lib/commands/PPSwerveControllerCommand.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/commands/PPSwerveControllerCommand.java
@@ -5,11 +5,11 @@ import java.util.function.Supplier;
 
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
-import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.Timer;
@@ -17,34 +17,15 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /**
- * A command that uses two PID controllers ({@link PIDController}) and a
- * ProfiledPIDController
- * ({@link ProfiledPIDController}) to follow a trajectory {@link PathPlannerTrajectory}
- * with a swerve drive.
- *
- * <p>
- * This command outputs the raw desired Swerve Module States
- * ({@link SwerveModuleState}) in an
- * array. The desired wheel and module rotation velocities should be taken from
- * those and used in
- * velocity PIDs.
- *
- * <p>
- * The robot angle controller does not follow the angle given by the trajectory
- * but rather goes
- * to the angle given in the final state of the trajectory.
- *
- * <p>
- * This class is provided by the NewCommands VendorDep
+ * Custom PathPlanner version of SwerveControllerCommand
  */
-@SuppressWarnings("MemberName")
 public class PPSwerveControllerCommand extends CommandBase {
-    private final Timer m_timer = new Timer();
-    private final PathPlannerTrajectory m_trajectory;
-    private final Supplier<Pose2d> m_pose;
-    private final SwerveDriveKinematics m_kinematics;
-    private final HolonomicDriveController m_controller;
-    private final Consumer<SwerveModuleState[]> m_outputModuleStates;
+    private final Timer timer = new Timer();
+    private final PathPlannerTrajectory trajectory;
+    private final Supplier<Pose2d> poseSupplier;
+    private final SwerveDriveKinematics kinematics;
+    private final PPHolonomicDriveController controller;
+    private final Consumer<SwerveModuleState[]> outputModuleStates;
 
     /**
      * Constructs a new PPSwerveControllerCommand that when executed will follow the
@@ -60,7 +41,7 @@ public class PPSwerveControllerCommand extends CommandBase {
      * nonstationary endstates.
      *
      * @param trajectory         The trajectory to follow.
-     * @param pose               A function that supplies the robot pose - use one
+     * @param poseSupplier       A function that supplies the robot pose - use one
      *                           of the odometry classes to
      *                           provide this.
      * @param kinematics         The kinematics for the robot drivetrain.
@@ -68,61 +49,91 @@ public class PPSwerveControllerCommand extends CommandBase {
      *                           robot's x position.
      * @param yController        The Trajectory Tracker PID controller for the
      *                           robot's y position.
-     * @param thetaController    The Trajectory Tracker PID controller for angle for
+     * @param rotationController    The Trajectory Tracker PID controller for angle for
      *                           the robot.
      * @param outputModuleStates The raw output module states from the position
      *                           controllers.
      * @param requirements       The subsystems to require.
      */
-    @SuppressWarnings("ParameterName")
     public PPSwerveControllerCommand(
             PathPlannerTrajectory trajectory,
-            Supplier<Pose2d> pose,
+            Supplier<Pose2d> poseSupplier,
             SwerveDriveKinematics kinematics,
             PIDController xController,
             PIDController yController,
-            ProfiledPIDController thetaController,
+            PIDController rotationController,
             Consumer<SwerveModuleState[]> outputModuleStates,
             Subsystem... requirements) {
-        m_trajectory = trajectory;
-        m_pose = pose;
-        m_kinematics = kinematics;
-
-        m_controller = new HolonomicDriveController(
-                xController,
-                yController,
-                thetaController);
-
-        m_outputModuleStates = outputModuleStates;
+        this.trajectory = trajectory;
+        this.poseSupplier = poseSupplier;
+        this.kinematics = kinematics;
+        this.controller = new PPHolonomicDriveController(xController, yController, rotationController);
+        this.outputModuleStates = outputModuleStates;
 
         addRequirements(requirements);
     }
 
-    @Override
-    public void initialize() {
-        m_timer.reset();
-        m_timer.start();
+    /**
+     * Constructs a new PPSwerveControllerCommand that when executed will follow the
+     * provided
+     * trajectory. This command will not return output voltages but rather raw
+     * module states from the
+     * position controllers which need to be put into a velocity PID.
+     *
+     * <p>
+     * Note: The controllers will *not* set the outputVolts to zero upon completion
+     * of the path-
+     * this is left to the user, since it is not appropriate for paths with
+     * nonstationary endstates.
+     *
+     * @param trajectory         The trajectory to follow.
+     * @param poseSupplier       A function that supplies the robot pose - use one
+     *                           of the odometry classes to
+     *                           provide this.
+     * @param kinematics         The kinematics for the robot drivetrain.
+     * @param xyController       The Trajectory Tracker PID controller for the
+     *                           robot's x and y position.
+     * @param rotationController    The Trajectory Tracker PID controller for angle for
+     *                           the robot.
+     * @param outputModuleStates The raw output module states from the position
+     *                           controllers.
+     * @param requirements       The subsystems to require.
+     */
+    public PPSwerveControllerCommand(
+            PathPlannerTrajectory trajectory,
+            Supplier<Pose2d> poseSupplier,
+            SwerveDriveKinematics kinematics,
+            PIDController xyController,
+            PIDController rotationController,
+            Consumer<SwerveModuleState[]> outputModuleStates,
+            Subsystem... requirements) {
+        this(trajectory, poseSupplier, kinematics, xyController, xyController, rotationController, outputModuleStates, requirements);
     }
 
     @Override
-    @SuppressWarnings("LocalVariableName")
+    public void initialize() {
+        this.timer.reset();
+        this.timer.start();
+    }
+
+    @Override
     public void execute() {
-        double curTime = m_timer.get();
-        var desiredState = (PathPlannerState) m_trajectory.sample(curTime);
+        double currentTime = this.timer.get();
+        PathPlannerState desiredState = (PathPlannerState) this.trajectory.sample(currentTime);
 
-        var targetChassisSpeeds = m_controller.calculate(m_pose.get(), desiredState, desiredState.holonomicRotation);
-        var targetModuleStates = m_kinematics.toSwerveModuleStates(targetChassisSpeeds);
+        ChassisSpeeds targetChassisSpeeds = this.controller.calculate(this.poseSupplier.get(), desiredState);
+        SwerveModuleState[] targetModuleStates = this.kinematics.toSwerveModuleStates(targetChassisSpeeds);
 
-        m_outputModuleStates.accept(targetModuleStates);
+        this.outputModuleStates.accept(targetModuleStates);
     }
 
     @Override
     public void end(boolean interrupted) {
-        m_timer.stop();
+        this.timer.stop();
     }
 
     @Override
     public boolean isFinished() {
-        return m_timer.hasElapsed(m_trajectory.getTotalTimeSeconds());
+        return this.timer.hasElapsed(this.trajectory.getTotalTimeSeconds());
     }
 }

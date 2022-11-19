676d42513879bcf04b6851ba5a2d23fb936e6acb
Michael Jansen <mjansen4857@gmail.com>
2022-11-19 15:57:04 -0500

    potentially fix macos build error

--- pathplannerlib/src/main/native/include/pathplanner/lib/auto/BaseAutoBuilder.h
+++ pathplannerlib/src/main/native/include/pathplanner/lib/auto/BaseAutoBuilder.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional>
+#include <unordered_map>
 #include <frc/geometry/Pose2d.h>
 #include <frc2/command/CommandBase.h>
 #include "pathplanner/lib/PathPlannerTrajectory.h"

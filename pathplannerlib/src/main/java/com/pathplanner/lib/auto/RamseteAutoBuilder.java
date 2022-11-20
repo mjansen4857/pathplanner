598b69f981d64dc53253aab554e0da9faaae30e9
Michael Jansen <mjansen4857@gmail.com>
2022-11-19 21:01:04 -0500

    cpp RamseteAutoBuilder

--- pathplannerlib/src/main/java/com/pathplanner/lib/auto/RamseteAutoBuilder.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/auto/RamseteAutoBuilder.java
@@ -2,12 +2,9 @@ package com.pathplanner.lib.auto;
 
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPRamseteCommand;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -18,11 +15,8 @@ import java.util.function.Supplier;
 
 public class RamseteAutoBuilder extends BaseAutoBuilder {
   private final RamseteController controller;
-  private final SimpleMotorFeedforward feedforward;
   private final DifferentialDriveKinematics kinematics;
-  private final Supplier<DifferentialDriveWheelSpeeds> speedsSupplier;
-  private final PIDConstants driveConstants;
-  private final BiConsumer<Double, Double> outputVolts;
+  private final BiConsumer<Double, Double> outputMetersPerSecond;
   private final Subsystem[] driveRequirements;
 
   /**
@@ -36,14 +30,7 @@ public class RamseteAutoBuilder extends BaseAutoBuilder {
    * @param resetPose A consumer that accepts a Pose2d to reset robot odometry. This will typically
    *     be called once at the beginning of an auto.
    * @param controller The RAMSETE controller used to follow the trajectory.
-   * @param feedforward The feedforward to use for the drive.
    * @param kinematics The kinematics for the robot drivetrain.
-   * @param speedsSupplier A function that supplies the speeds of the left and right sides of the
-   *     robot drive.
-   * @param driveConstants PID constants for the controllers that will correct for error on each
-   *     side of the drive train
-   * @param outputVolts A function that consumes the computed left and right outputs (in volts) for
-   *     the robot drive.
    * @param eventMap Map of event marker names to the commands that should run when reaching that
    *     marker.
    * @param driveRequirements The subsystems that the path following commands should require.
@@ -53,38 +40,21 @@ public class RamseteAutoBuilder extends BaseAutoBuilder {
       Supplier<Pose2d> poseSupplier,
       Consumer<Pose2d> resetPose,
       RamseteController controller,
-      SimpleMotorFeedforward feedforward,
       DifferentialDriveKinematics kinematics,
-      Supplier<DifferentialDriveWheelSpeeds> speedsSupplier,
-      PIDConstants driveConstants,
-      BiConsumer<Double, Double> outputVolts,
+      BiConsumer<Double, Double> outputMetersPerSecond,
       HashMap<String, Command> eventMap,
       Subsystem... driveRequirements) {
     super(poseSupplier, resetPose, eventMap, DrivetrainType.STANDARD);
 
     this.controller = controller;
-    this.feedforward = feedforward;
     this.kinematics = kinematics;
-    this.speedsSupplier = speedsSupplier;
-    this.driveConstants = driveConstants;
-    this.outputVolts = outputVolts;
+    this.outputMetersPerSecond = outputMetersPerSecond;
     this.driveRequirements = driveRequirements;
   }
 
   @Override
   public CommandBase followPath(PathPlannerTrajectory trajectory) {
     return new PPRamseteCommand(
-        trajectory,
-        poseSupplier,
-        controller,
-        feedforward,
-        kinematics,
-        speedsSupplier,
-        new PIDController(
-            driveConstants.kP, driveConstants.kI, driveConstants.kD, driveConstants.period),
-        new PIDController(
-            driveConstants.kP, driveConstants.kI, driveConstants.kD, driveConstants.period),
-        outputVolts,
-        driveRequirements);
+        trajectory, poseSupplier, controller, kinematics, outputMetersPerSecond, driveRequirements);
   }
 }

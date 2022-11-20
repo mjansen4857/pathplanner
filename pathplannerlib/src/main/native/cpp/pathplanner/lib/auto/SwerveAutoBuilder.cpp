f09a1fb4467c0242b0ca70977e13d2493c1d2d85
Michael Jansen <mjansen4857@gmail.com>
2022-11-19 19:41:13 -0500

    debugging this is the worst thing ever

--- pathplannerlib/src/main/native/cpp/pathplanner/lib/auto/SwerveAutoBuilder.cpp
+++ pathplannerlib/src/main/native/cpp/pathplanner/lib/auto/SwerveAutoBuilder.cpp
@@ -13,28 +13,14 @@ SwerveAutoBuilder::SwerveAutoBuilder(std::function<frc::Pose2d()> pose,
 		std::initializer_list<frc2::Subsystem*> driveRequirements) : BaseAutoBuilder(
 		pose, resetPose, eventMap, BaseAutoBuilder::DriveTrainType::HOLONOMIC), m_kinematics(
 		kinematics), m_translationConstants(translationConstants), m_rotationConstants(
-		rotationConstants), m_output(output) {
-	m_driveRequirements.insert(driveRequirements.begin(),
-			driveRequirements.end());
-}
+		rotationConstants), m_output(output), m_driveRequirements(
+		driveRequirements) {
 
-SwerveAutoBuilder::SwerveAutoBuilder(std::function<frc::Pose2d()> pose,
-		std::function<void(frc::Pose2d)> resetPose,
-		frc::SwerveDriveKinematics<4> kinematics,
-		PIDConstants translationConstants, PIDConstants rotationConstants,
-		std::function<void(std::array<frc::SwerveModuleState, 4>)> output,
-		std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap,
-		std::span<frc2::Subsystem* const > driveRequirements) : BaseAutoBuilder(
-		pose, resetPose, eventMap, BaseAutoBuilder::DriveTrainType::HOLONOMIC), m_kinematics(
-		kinematics), m_translationConstants(translationConstants), m_rotationConstants(
-		rotationConstants), m_output(output) {
-	m_driveRequirements.insert(driveRequirements.begin(),
-			driveRequirements.end());
 }
 
 frc2::CommandPtr SwerveAutoBuilder::followPath(
 		PathPlannerTrajectory trajectory) {
-	PPSwerveControllerCommand cmd(trajectory, m_pose, m_kinematics,
+	return PPSwerveControllerCommand(trajectory, m_pose, m_kinematics,
 			frc2::PIDController(m_translationConstants.m_kP,
 					m_translationConstants.m_kI, m_translationConstants.m_kD,
 					m_translationConstants.m_period),
@@ -43,9 +29,6 @@ frc2::CommandPtr SwerveAutoBuilder::followPath(
 					m_translationConstants.m_period),
 			frc2::PIDController(m_rotationConstants.m_kP,
 					m_rotationConstants.m_kI, m_rotationConstants.m_kD,
-					m_rotationConstants.m_period), m_output);
-
-	cmd.AddRequirements(m_driveRequirements);
-
-	return std::move(cmd).ToPtr();
+					m_rotationConstants.m_period), m_output,
+			m_driveRequirements).ToPtr();
 }
--- pathplannerlib/src/main/native/include/pathplanner/lib/auto/SwerveAutoBuilder.h
+++ pathplannerlib/src/main/native/include/pathplanner/lib/auto/SwerveAutoBuilder.h
@@ -38,35 +38,6 @@ public:
 			std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap,
 			std::initializer_list<frc2::Subsystem*> driveRequirements);
 
-	/**
-	 * Create an auto builder that will create command groups that will handle path following and
-	 * triggering events.
-	 *
-	 * <p>This auto builder will use PPSwerveControllerCommand to follow paths.
-	 *
-	 * @param pose A function that supplies the robot pose - use one of the odometry classes
-	 *     to provide this.
-	 * @param resetPose A consumer that accepts a Pose2d to reset robot odometry. This will typically
-	 *     be called once at the beginning of an auto.
-	 * @param kinematics The kinematics for the robot drivetrain.
-	 * @param translationConstants PID Constants for the controller that will correct for translation
-	 *     error
-	 * @param rotationConstants PID Constants for the controller that will correct for rotation error
-	 * @param output A function that takes raw output module states from path following
-	 *     commands
-	 * @param eventMap Map of event marker names to the commands that should run when reaching that
-	 *     marker.
-	 * @param driveRequirements The subsystems that the path following commands should require.
-	 *     Usually just a Drive subsystem.
-	 */
-	SwerveAutoBuilder(std::function<frc::Pose2d()> pose,
-			std::function<void(frc::Pose2d)> resetPose,
-			frc::SwerveDriveKinematics<4> kinematics,
-			PIDConstants translationConstants, PIDConstants rotationConstants,
-			std::function<void(std::array<frc::SwerveModuleState, 4>)> output,
-			std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap,
-			std::span<frc2::Subsystem* const > driveRequirements = { });
-
 	frc2::CommandPtr followPath(PathPlannerTrajectory trajectory) override;
 
 private:
@@ -74,6 +45,6 @@ private:
 	PIDConstants m_translationConstants;
 	PIDConstants m_rotationConstants;
 	std::function<void(std::array<frc::SwerveModuleState, 4>)> m_output;
-	wpi::SmallSet<frc2::Subsystem*, 4> m_driveRequirements;
+	std::initializer_list<frc2::Subsystem*> m_driveRequirements;
 };
 }

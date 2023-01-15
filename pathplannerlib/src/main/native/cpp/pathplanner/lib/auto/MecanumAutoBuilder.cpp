0f3d2c24f5dabfcd7db7790cb96dc2103cab9ee1
Michael Jansen <mjansen4857@gmail.com>
2023-01-15 14:56:15 -0500

    [cpp] add useAllianceColor overloads to auto builders

--- pathplannerlib/src/main/native/cpp/pathplanner/lib/auto/MecanumAutoBuilder.cpp
+++ pathplannerlib/src/main/native/cpp/pathplanner/lib/auto/MecanumAutoBuilder.cpp
@@ -9,12 +9,14 @@ MecanumAutoBuilder::MecanumAutoBuilder(std::function<frc::Pose2d()> pose,
 		PIDConstants translationConstants, PIDConstants rotationConstants,
 		std::function<void(frc::ChassisSpeeds)> output,
 		std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap,
-		std::initializer_list<frc2::Subsystem*> driveRequirements) : BaseAutoBuilder(
-		pose, resetPose, eventMap, BaseAutoBuilder::DriveTrainType::HOLONOMIC), m_kinematics(
+		std::initializer_list<frc2::Subsystem*> driveRequirements,
+		bool useAllianceColor) : BaseAutoBuilder(pose, resetPose, eventMap,
+		BaseAutoBuilder::DriveTrainType::HOLONOMIC), m_kinematics(
 		frc::Translation2d(), frc::Translation2d(), frc::Translation2d(),
 		frc::Translation2d()), m_translationConstants(translationConstants), m_rotationConstants(
 		rotationConstants), m_maxWheelVelocity(0_mps), m_outputChassisSpeeds(
-		output), m_driveRequirements(driveRequirements), m_useKinematics(false) {
+		output), m_driveRequirements(driveRequirements), m_useKinematics(false), m_useAllianceColor(
+		useAllianceColor) {
 }
 
 MecanumAutoBuilder::MecanumAutoBuilder(std::function<frc::Pose2d()> pose,
@@ -24,11 +26,13 @@ MecanumAutoBuilder::MecanumAutoBuilder(std::function<frc::Pose2d()> pose,
 		units::meters_per_second_t maxWheelVelocity,
 		std::function<void(frc::MecanumDriveWheelSpeeds)> output,
 		std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap,
-		std::initializer_list<frc2::Subsystem*> driveRequirements) : BaseAutoBuilder(
-		pose, resetPose, eventMap, BaseAutoBuilder::DriveTrainType::HOLONOMIC), m_kinematics(
-		kinematics), m_translationConstants(translationConstants), m_rotationConstants(
-		rotationConstants), m_maxWheelVelocity(maxWheelVelocity), m_outputVel(
-		output), m_driveRequirements(driveRequirements), m_useKinematics(true) {
+		std::initializer_list<frc2::Subsystem*> driveRequirements,
+		bool useAllianceColor) : BaseAutoBuilder(pose, resetPose, eventMap,
+		BaseAutoBuilder::DriveTrainType::HOLONOMIC), m_kinematics(kinematics), m_translationConstants(
+		translationConstants), m_rotationConstants(rotationConstants), m_maxWheelVelocity(
+		maxWheelVelocity), m_outputVel(output), m_driveRequirements(
+		driveRequirements), m_useKinematics(true), m_useAllianceColor(
+		useAllianceColor) {
 }
 
 frc2::CommandPtr MecanumAutoBuilder::followPath(
@@ -41,7 +45,7 @@ frc2::CommandPtr MecanumAutoBuilder::followPath(
 						m_translationConstants),
 				BaseAutoBuilder::pidControllerFromConstants(
 						m_rotationConstants), m_maxWheelVelocity, m_outputVel,
-				m_driveRequirements).ToPtr();
+				m_driveRequirements, m_useAllianceColor).ToPtr();
 	} else {
 		return PPMecanumControllerCommand(trajectory, m_pose,
 				BaseAutoBuilder::pidControllerFromConstants(
@@ -50,6 +54,6 @@ frc2::CommandPtr MecanumAutoBuilder::followPath(
 						m_translationConstants),
 				BaseAutoBuilder::pidControllerFromConstants(
 						m_rotationConstants), m_outputChassisSpeeds,
-				m_driveRequirements).ToPtr();
+				m_driveRequirements, m_useAllianceColor).ToPtr();
 	}
 }

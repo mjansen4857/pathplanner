7bef10cf06dcd74625fdc5d003cba07f87fe796d
Michael Jansen <mjansen4857@gmail.com>
2023-01-18 11:12:46 -0500

    [java] add logging callbacks to remaining commands

--- pathplannerlib/src/main/java/com/pathplanner/lib/commands/PPMecanumControllerCommand.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/commands/PPMecanumControllerCommand.java
@@ -10,14 +10,16 @@ import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.server.PathPlannerServer;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
 import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -33,9 +35,13 @@ public class PPMecanumControllerCommand extends CommandBase {
   private final Consumer<ChassisSpeeds> outputChassisSpeeds;
   private final boolean useKinematics;
   private final boolean useAllianceColor;
-  private final Field2d field = new Field2d();
 
   private PathPlannerTrajectory transformedTrajectory;
+
+  private static Consumer<PathPlannerTrajectory> logActiveTrajectory = null;
+  private static Consumer<Pose2d> logTargetPose = null;
+  private static BiConsumer<Translation2d, Rotation2d> logError =
+      PPMecanumControllerCommand::defaultLogError;
 
   /**
    * Constructs a new PPMecanumControllerCommand that when executed will follow the provided
@@ -225,8 +231,9 @@ public class PPMecanumControllerCommand extends CommandBase {
       transformedTrajectory = trajectory;
     }
 
-    SmartDashboard.putData("PPMecanumControllerCommand_field", this.field);
-    this.field.getObject("traj").setTrajectory(transformedTrajectory);
+    if (logActiveTrajectory != null) {
+      logActiveTrajectory.accept(transformedTrajectory);
+    }
 
     this.timer.reset();
     this.timer.start();
@@ -240,18 +247,21 @@ public class PPMecanumControllerCommand extends CommandBase {
     PathPlannerState desiredState = (PathPlannerState) transformedTrajectory.sample(currentTime);
 
     Pose2d currentPose = this.poseSupplier.get();
-    this.field.setRobotPose(currentPose);
+
     PathPlannerServer.sendPathFollowingData(
         new Pose2d(desiredState.poseMeters.getTranslation(), desiredState.holonomicRotation),
         currentPose);
 
-    SmartDashboard.putNumber(
-        "PPMecanumControllerCommand_xError", currentPose.getX() - desiredState.poseMeters.getX());
-    SmartDashboard.putNumber(
-        "PPMecanumControllerCommand_yError", currentPose.getY() - desiredState.poseMeters.getY());
-    SmartDashboard.putNumber(
-        "PPMecanumControllerCommand_rotationError",
-        currentPose.getRotation().getRadians() - desiredState.holonomicRotation.getRadians());
+    if (logTargetPose != null) {
+      logTargetPose.accept(
+          new Pose2d(desiredState.poseMeters.getTranslation(), desiredState.holonomicRotation));
+    }
+
+    if (logError != null) {
+      logError.accept(
+          currentPose.getTranslation().minus(desiredState.poseMeters.getTranslation()),
+          currentPose.getRotation().minus(desiredState.holonomicRotation));
+    }
 
     ChassisSpeeds targetChassisSpeeds = this.controller.calculate(currentPose, desiredState);
 
@@ -283,5 +293,32 @@ public class PPMecanumControllerCommand extends CommandBase {
   @Override
   public boolean isFinished() {
     return this.timer.hasElapsed(transformedTrajectory.getTotalTimeSeconds());
+  }
+
+  private static void defaultLogError(Translation2d translationError, Rotation2d rotationError) {
+    SmartDashboard.putNumber("PPMecanumControllerCommand/xErrorMeters", translationError.getX());
+    SmartDashboard.putNumber("PPMecanumControllerCommand/yErrorMeters", translationError.getY());
+    SmartDashboard.putNumber(
+        "PPMecanumControllerCommand/rotationErrorDegrees", rotationError.getDegrees());
+  }
+
+  /**
+   * Set custom logging callbacks for this command to use instead of the default configuration of
+   * pushing values to SmartDashboard
+   *
+   * @param logActiveTrajectory Consumer that accepts a PathPlannerTrajectory representing the
+   *     active path. This will be called whenever a PPMecanumControllerCommand starts
+   * @param logTargetPose Consumer that accepts a Pose2d representing the target pose while path
+   *     following
+   * @param logError BiConsumer that accepts a Translation2d and Rotation2d representing the error
+   *     while path following
+   */
+  public static void setLoggingCallbacks(
+      Consumer<PathPlannerTrajectory> logActiveTrajectory,
+      Consumer<Pose2d> logTargetPose,
+      BiConsumer<Translation2d, Rotation2d> logError) {
+    PPMecanumControllerCommand.logActiveTrajectory = logActiveTrajectory;
+    PPMecanumControllerCommand.logTargetPose = logTargetPose;
+    PPMecanumControllerCommand.logError = logError;
   }
 }
--- pathplannerlib/src/main/java/com/pathplanner/lib/commands/PPRamseteCommand.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/commands/PPRamseteCommand.java
@@ -6,15 +6,17 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /** Custom PathPlanner version of RamseteCommand */
@@ -31,12 +33,15 @@ public class PPRamseteCommand extends CommandBase {
   private final PIDController rightController;
   private final BiConsumer<Double, Double> output;
   private final boolean useAllianceColor;
-  private final Field2d field = new Field2d();
 
   private DifferentialDriveWheelSpeeds prevSpeeds;
   private double prevTime;
 
   private PathPlannerTrajectory transformedTrajectory;
+
+  private static Consumer<PathPlannerTrajectory> logActiveTrajectory = null;
+  private static Consumer<Pose2d> logTargetPose = null;
+  private static BiConsumer<Translation2d, Rotation2d> logError = PPRamseteCommand::defaultLogError;
 
   /**
    * Constructs a new PPRamseteCommand that, when executed, will follow the provided trajectory. PID
@@ -237,8 +242,9 @@ public class PPRamseteCommand extends CommandBase {
 
     this.prevTime = -1;
 
-    SmartDashboard.putData("PPRamseteCommand_field", this.field);
-    this.field.getObject("traj").setTrajectory(transformedTrajectory);
+    if (logActiveTrajectory != null) {
+      logActiveTrajectory.accept(transformedTrajectory);
+    }
 
     PathPlannerTrajectory.PathPlannerState initialState = transformedTrajectory.getInitialState();
 
@@ -274,17 +280,17 @@ public class PPRamseteCommand extends CommandBase {
     PathPlannerTrajectory.PathPlannerState desiredState =
         (PathPlannerTrajectory.PathPlannerState) transformedTrajectory.sample(currentTime);
 
-    this.field.setRobotPose(currentPose);
     PathPlannerServer.sendPathFollowingData(desiredState.poseMeters, currentPose);
 
-    SmartDashboard.putNumber(
-        "PPRamseteCommand_xError", currentPose.getX() - desiredState.poseMeters.getX());
-    SmartDashboard.putNumber(
-        "PPRamseteCommand_yError", currentPose.getY() - desiredState.poseMeters.getY());
-    SmartDashboard.putNumber(
-        "PPRamseteCommand_rotationError",
-        currentPose.getRotation().getRadians()
-            - desiredState.poseMeters.getRotation().getRadians());
+    if (logTargetPose != null) {
+      logTargetPose.accept(desiredState.poseMeters);
+    }
+
+    if (logError != null) {
+      logError.accept(
+          currentPose.getTranslation().minus(desiredState.poseMeters.getTranslation()),
+          currentPose.getRotation().minus(desiredState.poseMeters.getRotation()));
+    }
 
     DifferentialDriveWheelSpeeds targetWheelSpeeds =
         this.kinematics.toWheelSpeeds(this.controller.calculate(currentPose, desiredState));
@@ -333,5 +339,31 @@ public class PPRamseteCommand extends CommandBase {
   @Override
   public boolean isFinished() {
     return this.timer.hasElapsed(transformedTrajectory.getTotalTimeSeconds());
+  }
+
+  private static void defaultLogError(Translation2d translationError, Rotation2d rotationError) {
+    SmartDashboard.putNumber("PPRamseteCommand/xErrorMeters", translationError.getX());
+    SmartDashboard.putNumber("PPRamseteCommand/yErrorMeters", translationError.getY());
+    SmartDashboard.putNumber("PPRamseteCommand/rotationErrorDegrees", rotationError.getDegrees());
+  }
+
+  /**
+   * Set custom logging callbacks for this command to use instead of the default configuration of
+   * pushing values to SmartDashboard
+   *
+   * @param logActiveTrajectory Consumer that accepts a PathPlannerTrajectory representing the
+   *     active path. This will be called whenever a PPRamseteCommand starts
+   * @param logTargetPose Consumer that accepts a Pose2d representing the target pose while path
+   *     following
+   * @param logError BiConsumer that accepts a Translation2d and Rotation2d representing the error
+   *     while path following
+   */
+  public static void setLoggingCallbacks(
+      Consumer<PathPlannerTrajectory> logActiveTrajectory,
+      Consumer<Pose2d> logTargetPose,
+      BiConsumer<Translation2d, Rotation2d> logError) {
+    PPRamseteCommand.logActiveTrajectory = logActiveTrajectory;
+    PPRamseteCommand.logTargetPose = logTargetPose;
+    PPRamseteCommand.logError = logError;
   }
 }

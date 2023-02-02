804191077a88c2b81c8461da22467b6b5ae1e472
George Jurgiel <59634069+gcjurgiel@users.noreply.github.com>
2023-02-02 11:56:04 -0600

    Fix drivetrain type initialization (#238)
    * Fix drivetrain type initialization
    
    * Format

--- pathplannerlib/src/main/native/cpp/pathplanner/lib/auto/BaseAutoBuilder.cpp
+++ pathplannerlib/src/main/native/cpp/pathplanner/lib/auto/BaseAutoBuilder.cpp
@@ -11,7 +11,8 @@ BaseAutoBuilder::BaseAutoBuilder(std::function<frc::Pose2d()> pose,
 		std::unordered_map<std::string, std::shared_ptr<frc2::Command>> eventMap,
 		DriveTrainType drivetrainType, bool useAllianceColor) : m_pose(
 		std::move(pose)), m_resetPose(std::move(resetPose)), m_eventMap(
-		eventMap), m_useAllianceColor(useAllianceColor) {
+		eventMap), m_drivetrainType(drivetrainType), m_useAllianceColor(
+		useAllianceColor) {
 }
 
 frc2::CommandPtr BaseAutoBuilder::followPathGroup(

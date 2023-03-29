2f79e61a5046caa093b44a4182e7e2aa309f9356
Michael Jansen <mjansen4857@gmail.com>
2023-03-29 12:04:00 -0400

    make analysis happy

--- lib/services/log.dart
+++ lib/services/log.dart
@@ -4,8 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-// ignore: implementation_imports
-import 'package:logger/src/outputs/file_output.dart';
 
 class Log {
   static Logger? logger;

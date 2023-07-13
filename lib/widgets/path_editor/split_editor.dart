86922c4e40b4b6d92930d0ef6bbefd14c2150134
Michael Jansen <mjansen4857@gmail.com>
2023-07-13 00:30:16 -0400

    editor tree mockup

--- lib/widgets/path_editor/split_editor.dart
+++ lib/widgets/path_editor/split_editor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_treeview/flutter_simple_treeview.dart';
 import 'package:multi_split_view/multi_split_view.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -52,9 +53,187 @@ class _SplitEditorState extends State<SplitEditor> {
           widget.prefs.setDouble(
               'editorLeftWeight', _controller.areas[0].weight ?? 0.5);
         },
-        children: const [
-          Placeholder(),
-          Placeholder(),
+        children: [
+          const Placeholder(),
+          Card(
+            margin: const EdgeInsets.all(0),
+            elevation: 4.0,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                bottomLeft: Radius.circular(12),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: TreeView(
+                  nodes: [
+                    TreeNode(
+                      content: const Text('Path'),
+                      children: [
+                        TreeNode(
+                          content: const Text('Start Point'),
+                          children: [
+                            TreeNode(
+                              content: const Text(
+                                  'X Position          Y Position          Heading'),
+                            ),
+                            TreeNode(
+                              content: const Text('Leading Control Length'),
+                            ),
+                            TreeNode(
+                              content: const Text('Stop Event'),
+                              children: [
+                                TreeNode(
+                                  content: const Text('Nested Commands'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        TreeNode(
+                          content: const Text('Waypoint 1'),
+                          children: [
+                            TreeNode(
+                              content: const Text(
+                                  'X Position          Y Position          Heading'),
+                            ),
+                            TreeNode(
+                              content: const Text(
+                                  'Trailing Control Length          Leading Control Length'),
+                            ),
+                            TreeNode(
+                              content: const Text(
+                                  'Is Reversal          Is Stop Point'),
+                            ),
+                          ],
+                        ),
+                        TreeNode(
+                          content: const Text('End Point'),
+                          children: [
+                            TreeNode(
+                              content: const Text(
+                                  'X Position          Y Position          Heading'),
+                            ),
+                            TreeNode(
+                              content: const Text(
+                                  'Trailing Control Length          Goal End Velocity'),
+                            ),
+                            TreeNode(
+                              content: const Text('Stop Event'),
+                              children: [
+                                TreeNode(
+                                  content: const Text('Nested Commands'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        TreeNode(
+                          content: const Text('Global Constraints'),
+                          children: [
+                            TreeNode(
+                              content: const Text(
+                                  'Max Velocity          Max Acceleration'),
+                            ),
+                            TreeNode(
+                              content: const Text(
+                                  'Max Angular Vel          Max Angular Accel'),
+                            ),
+                          ],
+                        ),
+                        TreeNode(
+                          content: const Text('Is Reversed'),
+                        ),
+                      ],
+                    ),
+                    TreeNode(
+                      content: const Text('Rotation Targets'),
+                      children: [
+                        TreeNode(
+                          content: const Text(
+                              'Rotation Target 0          Position          Rotation'),
+                        ),
+                        TreeNode(
+                          content: const Text('Stop Point 0          Rotation'),
+                        ),
+                        TreeNode(
+                          content: const Text('End Point          Rotation'),
+                        ),
+                      ],
+                    ),
+                    TreeNode(
+                      content: const Text('Event Markers'),
+                      children: [
+                        TreeNode(
+                          content: const Text('Event Marker 0'),
+                          children: [
+                            TreeNode(
+                              content: const Text(
+                                  'Position          Min Trigger Distance'),
+                            ),
+                            TreeNode(
+                              content: const Text('Nested Commands'),
+                            ),
+                          ],
+                        ),
+                        TreeNode(
+                          content: const Text('Custom Name'),
+                          children: [
+                            TreeNode(
+                              content: const Text(
+                                  'Position          Min Trigger Distance'),
+                            ),
+                            TreeNode(
+                              content: const Text('Nested Commands'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    TreeNode(
+                      content: const Text('Constraint Zones'),
+                      children: [
+                        TreeNode(
+                          content: const Text('Constraint Zone 0'),
+                          children: [
+                            TreeNode(
+                              content: const Text('Range Slider'),
+                            ),
+                            TreeNode(
+                              content: const Text(
+                                  'Max Velocity          Max Acceleration'),
+                            ),
+                            TreeNode(
+                              content: const Text(
+                                  'Max Angular Vel          Max Angular Accel'),
+                            ),
+                          ],
+                        ),
+                        TreeNode(
+                          content: const Text('Custom Name'),
+                          children: [
+                            TreeNode(
+                              content: const Text('Range Slider'),
+                            ),
+                            TreeNode(
+                              content: const Text(
+                                  'Max Velocity          Max Acceleration'),
+                            ),
+                            TreeNode(
+                              content: const Text(
+                                  'Max Angular Vel          Max Angular Accel'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );

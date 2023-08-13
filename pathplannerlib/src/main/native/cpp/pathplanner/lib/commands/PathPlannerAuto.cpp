f8befc6ab235cec74d32becfc0b6e4fed2cab752
Michael Jansen <mjansen4857@gmail.com>
2023-08-13 00:01:38 -0400

    c++ pathgroup functions

--- pathplannerlib/src/main/native/cpp/pathplanner/lib/commands/PathPlannerAuto.cpp
+++ pathplannerlib/src/main/native/cpp/pathplanner/lib/commands/PathPlannerAuto.cpp
@@ -1,6 +1,8 @@
 #include "pathplanner/lib/commands/PathPlannerAuto.h"
 #include "pathplanner/lib/auto/AutoBuilder.h"
 #include "pathplanner/lib/util/PPLibTelemetry.h"
+#include <frc/Filesystem.h>
+#include <wpi/raw_istream.h>
 
 using namespace pathplanner;
 
@@ -12,6 +14,41 @@ PathPlannerAuto::PathPlannerAuto(std::string autoName) {
 
 	m_autoCommand = AutoBuilder::buildAuto(autoName).Unwrap();
 	m_requirements = m_autoCommand->GetRequirements();
+}
+
+std::vector<std::shared_ptr<PathPlannerPath>> PathPlannerAuto::getPathGroupFromAutoFile(
+		std::string autoName) {
+	const std::string filePath = frc::filesystem::GetDeployDirectory()
+			+ "/pathplanner/autos/" + autoName + ".auto";
+
+	std::error_code error_code;
+	wpi::raw_fd_istream input { filePath, error_code };
+
+	if (error_code) {
+		throw std::runtime_error("Cannot open file: " + filePath);
+	}
+
+	wpi::json json;
+	input >> json;
+
+	return pathsFromCommandJson(json.at("command"));
+}
+
+frc::Pose2d PathPlannerAuto::getStartingPoseFromAutoFile(std::string autoName) {
+	const std::string filePath = frc::filesystem::GetDeployDirectory()
+			+ "/pathplanner/autos/" + autoName + ".auto";
+
+	std::error_code error_code;
+	wpi::raw_fd_istream input { filePath, error_code };
+
+	if (error_code) {
+		throw std::runtime_error("Cannot open file: " + filePath);
+	}
+
+	wpi::json json;
+	input >> json;
+
+	return AutoBuilder::getStartingPoseFromJson(json.at("startingPose"));
 }
 
 void PathPlannerAuto::Initialize() {
@@ -28,4 +65,25 @@ bool PathPlannerAuto::IsFinished() {
 
 void PathPlannerAuto::End(bool interrupted) {
 	m_autoCommand->End(interrupted);
+}
+
+std::vector<std::shared_ptr<PathPlannerPath>> PathPlannerAuto::pathsFromCommandJson(
+		const wpi::json &json) {
+	std::vector < std::shared_ptr < PathPlannerPath >> paths;
+
+	std::string type = json.at("type").get<std::string>();
+	wpi::json::const_reference data = json.at("data");
+
+	if (type == "path") {
+		std::string pathName = data.at("pathName").get<std::string>();
+		paths.push_back(PathPlannerPath::fromPathFile(pathName));
+	} else if (type == "sequential" || type == "parallel" || type == "race"
+			|| type == "deadline") {
+		for (wpi::json::const_reference cmdJson : data.at("commands")) {
+			auto cmdPaths = pathsFromCommandJson(cmdJson);
+			paths.insert(paths.end(), cmdPaths.begin(), cmdPaths.end());
+		}
+	}
+
+	return paths;
 }

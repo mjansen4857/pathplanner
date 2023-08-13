658c6f6e7361ff017f62a908a4bce3559e455dc6
Michael Jansen <mjansen4857@gmail.com>
2023-08-12 23:41:12 -0400

    get path group from a static PathPlannerAuto method instead of different class

--- pathplannerlib/src/main/java/com/pathplanner/lib/commands/PathPlannerAuto.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/commands/PathPlannerAuto.java
@@ -1,9 +1,19 @@
 package com.pathplanner.lib.commands;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.PPLibTelemetry;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 /** A command that loads and runs an autonomous routine built using PathPlanner. */
 public class PathPlannerAuto extends Command {
@@ -25,6 +35,58 @@ public class PathPlannerAuto extends Command {
     this.autoCommand = AutoBuilder.buildAuto(autoName);
     m_requirements = autoCommand.getRequirements();
     PPLibTelemetry.registerHotReloadAuto(autoName, this);
+  }
+
+  /**
+   * Get the starting pose from the given auto file
+   *
+   * @param autoName Name of the auto to get the pose from
+   * @return Starting pose from the given auto
+   */
+  public static Pose2d getStaringPoseFromAutoFile(String autoName) {
+    try (BufferedReader br =
+        new BufferedReader(
+            new FileReader(
+                new File(
+                    Filesystem.getDeployDirectory(), "pathplanner/autos/" + autoName + ".auto")))) {
+      StringBuilder fileContentBuilder = new StringBuilder();
+      String line;
+      while ((line = br.readLine()) != null) {
+        fileContentBuilder.append(line);
+      }
+
+      String fileContent = fileContentBuilder.toString();
+      JSONObject json = (JSONObject) new JSONParser().parse(fileContent);
+      return AutoBuilder.getStartingPoseFromJson((JSONObject) json.get("startingPose"));
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage());
+    }
+  }
+
+  /**
+   * Get a list of every path in the given auto (depth first)
+   *
+   * @param autoName Name of the auto to get the path group from
+   * @return List of paths in the auto
+   */
+  public static List<PathPlannerPath> getPathGroupFromAutoFile(String autoName) {
+    try (BufferedReader br =
+        new BufferedReader(
+            new FileReader(
+                new File(
+                    Filesystem.getDeployDirectory(), "pathplanner/autos/" + autoName + ".auto")))) {
+      StringBuilder fileContentBuilder = new StringBuilder();
+      String line;
+      while ((line = br.readLine()) != null) {
+        fileContentBuilder.append(line);
+      }
+
+      String fileContent = fileContentBuilder.toString();
+      JSONObject json = (JSONObject) new JSONParser().parse(fileContent);
+      return pathsFromCommandJson((JSONObject) json.get("command"));
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage());
+    }
   }
 
   /**
@@ -56,5 +118,26 @@ public class PathPlannerAuto extends Command {
   @Override
   public void end(boolean interrupted) {
     autoCommand.end(interrupted);
+  }
+
+  private static List<PathPlannerPath> pathsFromCommandJson(JSONObject commandJson) {
+    List<PathPlannerPath> paths = new ArrayList<>();
+
+    String type = (String) commandJson.get("type");
+    JSONObject data = (JSONObject) commandJson.get("data");
+
+    if (type.equals("path")) {
+      String pathName = (String) data.get("pathName");
+      paths.add(PathPlannerPath.fromPathFile(pathName));
+    } else if (type.equals("sequential")
+        || type.equals("parallel")
+        || type.equals("race")
+        || type.equals("deadline")) {
+      for (var cmdJson : (JSONArray) data.get("commands")) {
+        paths.addAll(pathsFromCommandJson((JSONObject) cmdJson));
+      }
+    }
+
+    return paths;
   }
 }

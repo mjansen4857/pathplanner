10c951e0ebcacce31525866ba822dd5eaa7ff2aa
Michael Jansen <mjansen4857@gmail.com>
2023-08-19 22:17:57 -0400

    add animations to welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -3,41 +3,14 @@ import 'dart:ui';
 
 import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:pathplanner/widgets/custom_appbar.dart';
 import 'package:pathplanner/widgets/field_image.dart';
 
-class WelcomePage extends StatefulWidget {
+class WelcomePage extends StatelessWidget {
   final String appVersion;
 
   const WelcomePage({required this.appVersion, super.key});
-
-  @override
-  State<WelcomePage> createState() => _WelcomePageState();
-}
-
-class _WelcomePageState extends State<WelcomePage>
-    with TickerProviderStateMixin {
-  late AnimationController _welcomeController;
-  late Animation<double> _scaleAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _welcomeController = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 500));
-    _scaleAnimation =
-        CurvedAnimation(parent: _welcomeController, curve: Curves.ease);
-
-    _welcomeController.forward();
-  }
-
-  @override
-  void dispose() {
-    _welcomeController.dispose();
-
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,61 +31,78 @@ class _WelcomePageState extends State<WelcomePage>
               padding: const EdgeInsets.all(48),
               child: FieldImage.defaultField.getWidget(),
             ),
-          ),
+          ).animate().scaleXY(duration: 300.ms, curve: Curves.easeInOut),
           Center(
             child: BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-              child: Container(
-                color: Colors.black.withOpacity(0.15),
-                child: ScaleTransition(
-                  scale: _scaleAnimation,
-                  child: Row(
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 250,
-                            height: 250,
-                            child: Image.asset('images/icon.png'),
+                      SizedBox(
+                        width: 250,
+                        height: 250,
+                        child: Image.asset('images/icon.png'),
+                      )
+                          .animate()
+                          .fadeIn(
+                              delay: 400.ms,
+                              duration: 200.ms,
+                              curve: Curves.easeInOut)
+                          .scaleXY()
+                          .shimmer(delay: 2.seconds, duration: 300.ms),
+                      Text(
+                        'PathPlanner',
+                        style: TextStyle(
+                            fontSize: 48, color: colorScheme.onSurface),
+                      )
+                          .animate()
+                          .fadeIn(
+                              delay: 600.ms,
+                              duration: 400.ms,
+                              curve: Curves.easeInOut)
+                          .slide(begin: const Offset(0, 0.3)),
+                      Text(
+                        'v$appVersion',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: colorScheme.secondary,
+                        ),
+                      )
+                          .animate()
+                          .fadeIn(
+                              delay: 800.ms,
+                              duration: 400.ms,
+                              curve: Curves.easeInOut)
+                          .slide(begin: const Offset(0, 0.3)),
+                      const SizedBox(height: 64),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: colorScheme.surface,
+                          foregroundColor: colorScheme.primary,
+                          elevation: 4.0,
+                        ),
+                        onPressed: () {
+                          _showOpenProjectDialog(context);
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: Text(
+                            'Open Robot Project',
+                            style: TextStyle(fontSize: 24),
                           ),
-                          Text(
-                            'PathPlanner',
-                            style: TextStyle(
-                                fontSize: 48, color: colorScheme.onSurface),
-                          ),
-                          Text(
-                            'v${widget.appVersion}',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: colorScheme.secondary,
-                            ),
-                          ),
-                          const SizedBox(height: 64),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: colorScheme.surfaceVariant,
-                              foregroundColor: colorScheme.onSurfaceVariant,
-                            ),
-                            onPressed: () {
-                              _showOpenProjectDialog(context);
-                            },
-                            child: const Padding(
-                              padding: EdgeInsets.all(6.0),
-                              child: Text(
-                                'Open Robot Project',
-                                style: TextStyle(fontSize: 24),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ).animate().fadeIn(
+                          delay: 1.5.seconds,
+                          duration: 500.ms,
+                          curve: Curves.easeInOut),
                     ],
                   ),
-                ),
+                ],
               ),
             ),
           ),
@@ -127,7 +117,7 @@ class _WelcomePageState extends State<WelcomePage>
       initialDirectory: Directory.current.path,
     );
 
-    if (!mounted) return;
+    if (!context.mounted) return;
 
     if (projectFolder != null) {
       Navigator.pop(context, projectFolder);

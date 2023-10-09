b474d544701afa1aba42a91ff4ca8a6e2a1fc226
Michael Jansen <mjansen4857@gmail.com>
2023-10-09 13:28:57 -0400

    Add registerCommand overload that takes a CommandPtr

--- pathplannerlib/src/main/native/cpp/pathplanner/lib/auto/NamedCommands.cpp
+++ pathplannerlib/src/main/native/cpp/pathplanner/lib/auto/NamedCommands.cpp
@@ -5,15 +5,6 @@ using namespace pathplanner;
 
 std::unordered_map<std::string, std::shared_ptr<frc2::Command>> NamedCommands::namedCommands;
 
-void NamedCommands::registerCommand(std::string name,
-		std::shared_ptr<frc2::Command> command) {
-	NamedCommands::namedCommands.emplace(name, command);
-}
-
-bool NamedCommands::hasCommand(std::string name) {
-	return NamedCommands::namedCommands.contains(name);
-}
-
 frc2::CommandPtr NamedCommands::getCommand(std::string name) {
 	if (NamedCommands::hasCommand(name)) {
 		return CommandUtil::wrappedEventCommand(

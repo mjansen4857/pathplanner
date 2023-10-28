41f64c81a42b2655b9aa5e8c626d146d2ddfa907
Michael Jansen <mjansen4857@gmail.com>
2023-10-27 18:17:48 -0700

    Add seek bar and pause/play button for path/auto simulations (#438)
    * add preview seekbar to path/auto editors
    
    * adjust seekbar elevation
    
    * add seek bar tests

---
+++ lib/widgets/editor/preview_seekbar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class PreviewSeekbar extends StatefulWidget {
+  final AnimationController previewController;
+
+  const PreviewSeekbar({
+    super.key,
+    required this.previewController,
+  });
+
+  @override
+  State<PreviewSeekbar> createState() => _PreviewSeekbarState();
+}
+
+class _PreviewSeekbarState extends State<PreviewSeekbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+        child: Card(
+          elevation: 2.0,
+          child: SizedBox(
+            height: 32,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(width: 8),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      if (widget.previewController.isAnimating) {
+                        widget.previewController.stop();
+                      } else {
+                        widget.previewController.repeat();
+                      }
+                    });
+                  },
+                  icon: widget.previewController.isAnimating
+                      ? const Icon(Icons.pause)
+                      : const Icon(Icons.play_arrow),
+                  visualDensity: VisualDensity.compact,
+                ),
+                Expanded(
+                  child: AnimatedBuilder(
+                      animation: widget.previewController.view,
+                      builder: (context, _) {
+                        return Slider(
+                          value: widget.previewController.view.value,
+                          focusNode: FocusNode(
+                            skipTraversal: true,
+                            canRequestFocus: false,
+                          ),
+                          onChanged: (value) {
+                            if (widget.previewController.isAnimating) {
+                              setState(() {
+                                widget.previewController.stop();
+                              });
+                            }
+
+                            widget.previewController.value = value;
+                          },
+                        );
+                      }),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

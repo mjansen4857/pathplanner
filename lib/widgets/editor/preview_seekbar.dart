3e03712b89989f738122044c17265595826e2dc1
swirl <swurl@swurl.xyz>
2023-10-28 19:55:12 -0400

    broken trim bar
    Signed-off-by: swirl <swurl@swurl.xyz>

--- lib/widgets/editor/preview_seekbar.dart
+++ lib/widgets/editor/preview_seekbar.dart
@@ -13,6 +13,8 @@ class PreviewSeekbar extends StatefulWidget {
 }
 
 class _PreviewSeekbarState extends State<PreviewSeekbar> {
+  RangeValues rangeValues = const RangeValues(0.0, 1.0);
+
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -22,48 +24,78 @@ class _PreviewSeekbarState extends State<PreviewSeekbar> {
         child: Card(
           elevation: 2.0,
           child: SizedBox(
-            height: 32,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+            height: 80,
+            child: Column(
               children: [
-                const SizedBox(width: 8),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      if (widget.previewController.isAnimating) {
-                        widget.previewController.stop();
-                      } else {
-                        widget.previewController.repeat();
-                      }
-                    });
-                  },
-                  icon: widget.previewController.isAnimating
-                      ? const Icon(Icons.pause)
-                      : const Icon(Icons.play_arrow),
-                  visualDensity: VisualDensity.compact,
-                ),
-                Expanded(
-                  child: AnimatedBuilder(
-                      animation: widget.previewController.view,
-                      builder: (context, _) {
-                        return Slider(
-                          value: widget.previewController.view.value,
-                          focusNode: FocusNode(
-                            skipTraversal: true,
-                            canRequestFocus: false,
-                          ),
-                          onChanged: (value) {
-                            if (widget.previewController.isAnimating) {
-                              setState(() {
-                                widget.previewController.stop();
-                              });
-                            }
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(width: 8),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          if (widget.previewController.isAnimating) {
+                            widget.previewController.stop();
+                          } else {
+                            widget.previewController.repeat(min: rangeValues.start, max: rangeValues.end);
+                          }
+                        });
+                      },
+                      icon: widget.previewController.isAnimating
+                          ? const Icon(Icons.pause)
+                          : const Icon(Icons.play_arrow),
+                      visualDensity: VisualDensity.compact,
+                    ),
+                    Expanded(
+                      child: AnimatedBuilder(
+                          animation: widget.previewController.view,
+                          builder: (context, _) {
+                            return Slider(
+                              value: widget.previewController.view.value,
+                              focusNode: FocusNode(
+                                skipTraversal: true,
+                                canRequestFocus: false,
+                              ),
+                              onChanged: (value) {
+                                if (widget.previewController.isAnimating) {
+                                  setState(() {
+                                    widget.previewController.stop();
+                                  });
+                                }
 
-                            widget.previewController.value = value;
-                          },
-                        );
-                      }),
+                                widget.previewController.value = value;
+                              },
+                            );
+                          }),
+                    ),
+                  ],
                 ),
+                const SizedBox(height: 20),
+                // Row(
+                //   children: [
+                    Expanded(
+                      child: AnimatedBuilder(
+                          animation: widget.previewController.view,
+                          builder: (context, _) {
+                            return RangeSlider(
+                              values: rangeValues,
+                              labels: RangeLabels(
+                                rangeValues.start.toString(),
+                                rangeValues.end.toString(),
+                              ),
+                              onChanged: (RangeValues values) {
+                                if (widget.previewController.isAnimating) {
+                                  setState(() {
+                                    widget.previewController.stop();
+                                    rangeValues = values;
+                                  });
+                                }
+                              },
+                            );
+                          }),
+                    ),
+                //   ],
+                // ),
               ],
             ),
           ),

0d9cf1e8a1963bafab67b79daa155af46f8006b2
Michael Jansen <mjansen4857@gmail.com>
2023-12-05 13:19:38 -0500

    add pathfinding commands

--- pathplannerlib-python/src/pathplannerlib/commands.py
+++ pathplannerlib-python/src/pathplannerlib/commands.py
@@ -1,16 +1,18 @@
 import math
 
-from controller import *
-from path import PathPlannerPath, EventMarker
-from trajectory import PathPlannerTrajectory
-from telemetry import PPLibTelemetry
+from .controller import *
+from .path import PathPlannerPath, EventMarker, GoalEndState, PathConstraints
+from .trajectory import PathPlannerTrajectory
+from .telemetry import PPLibTelemetry
 from .logging import PathPlannerLogging
+from .geometry_util import floatLerp
 from wpimath.geometry import Pose2d
 from wpimath.kinematics import ChassisSpeeds
 from wpilib import Timer
 from commands2 import Command, Subsystem
 from typing import Callable, Tuple, List
-from config import ReplanningConfig, HolonomicPathFollowerConfig
+from .config import ReplanningConfig, HolonomicPathFollowerConfig
+from .pathfinding import Pathfinding
 
 
 class FollowPathWithEvents(Command):
@@ -274,3 +276,244 @@ class FollowPathLTV(FollowPathCommand):
         super().__init__(path, pose_supplier, speeds_supplier, output_robot_relative,
                          PPLTVController(qelems, relems, dt),
                          replanning_config, *requirements)
+
+
+class PathfindingCommand(Command):
+    _timer: Timer = Timer()
+    _targetPath: Union[PathPlannerPath, None]
+    _targetPose: Pose2d
+    _goalEndState: GoalEndState
+    _constraints: PathConstraints
+    _poseSupplier: Callable[[], Pose2d]
+    _speedsSupplier: Callable[[], ChassisSpeeds]
+    _output: Callable[[ChassisSpeeds], None]
+    _controller: PathFollowingController
+    _rotationDelayDistance: float
+    _replanningConfig: ReplanningConfig
+
+    _currentPath: Union[PathPlannerPath, None]
+    _currentTrajectory: Union[PathPlannerTrajectory, None]
+    _startingPose: Pose2d
+
+    _timeOffset: float = 0
+
+    def __init__(self, constraints: PathConstraints, pose_supplier: Callable[[], Pose2d],
+                 speeds_supplier: Callable[[], ChassisSpeeds], output_robot_relative: Callable[[ChassisSpeeds], None],
+                 controller: PathFollowingController, replanning_config: ReplanningConfig, *requirements: Subsystem,
+                 rotation_delay_distance: float = 0.0, target_path: PathPlannerPath = None, target_pose: Pose2d = None,
+                 goal_end_vel: float = 0):
+        super().__init__()
+        if target_path is None and target_pose is None:
+            raise ValueError('Either target_path or target_pose must be specified for PathfindingCommand')
+
+        self.addRequirements(*requirements)
+
+        Pathfinding.ensureInitialized()
+
+        self._constraints = constraints
+        self._controller = controller
+        self._poseSupplier = pose_supplier
+        self._speedsSupplier = speeds_supplier
+        self._output = output_robot_relative
+        self._rotationDelayDistance = rotation_delay_distance
+        self._replanningConfig = replanning_config
+
+        if target_path is not None:
+            targetRotation = Rotation2d()
+            for p in target_path.getAllPathPoints():
+                if p.rotationTarget is not None:
+                    targetRotation = p.rotationTarget.target
+                    break
+            self._targetPath = target_path
+            self._targetPose = Pose2d(target_path.getPoint(0).position, targetRotation)
+            self._goalEndState = GoalEndState(target_path.getGlobalConstraints().maxVelocityMps, targetRotation, True)
+        else:
+            self._targetPath = None
+            self._targetPose = target_pose
+            self._goalEndState = GoalEndState(goal_end_vel, target_pose.rotation(), True)
+
+    def initialize(self):
+        self._currentTrajectory = None
+        self._timeOffset = 0.0
+
+        currentPose = self._poseSupplier()
+
+        self._controller.reset(currentPose, self._speedsSupplier())
+
+        if self._targetPath is not None:
+            self._targetPose = Pose2d(self._targetPath.getPoint(0).position, self._goalEndState.rotation)
+
+        if currentPose.translation().distance(self._targetPose.translation()) < 0.25:
+            self.cancel()
+        else:
+            Pathfinding.setStartPosition(currentPose.translation())
+            Pathfinding.setGoalPosition(self._targetPose.translation())
+
+        self._startingPose = currentPose
+
+    def execute(self):
+        currentPose = self._poseSupplier()
+        currentSpeeds = self._speedsSupplier()
+
+        PathPlannerLogging.logCurrentPose(currentPose)
+        PPLibTelemetry.setCurrentPose(currentPose)
+
+        # Skip new paths if we are close to the end
+        skipUpdates = self._currentTrajectory is not None and currentPose.translation().distance(
+            self._currentTrajectory.getEndState().positionMeters) < 2.0
+
+        if not skipUpdates and Pathfinding.isNewPathAvailable():
+            self._currentPath = Pathfinding.getCurrentPath(self._constraints, self._goalEndState)
+
+            if self._currentPath is not None:
+                self._currentTrajectory = PathPlannerTrajectory(self._currentPath, currentSpeeds,
+                                                                currentPose.rotation())
+
+                # Find the two closest states in front of and behind robot
+                closestState1Idx = 0
+                closestState2Idx = 1
+                while True:
+                    closest2Dist = self._currentTrajectory.getState(closestState2Idx).positionMeters.distance(
+                        currentPose.translation())
+                    nextDist = self._currentTrajectory.getState(closestState2Idx + 1).positionMeters.distance(
+                        currentPose.translation())
+
+                    if nextDist < closest2Dist:
+                        closestState1Idx += 1
+                        closestState2Idx += 1
+                    else:
+                        break
+
+                # Use the closest 2 states to interpolate what the time offset should be
+                # This will account for the delay in pathfinding
+                closestState1 = self._currentTrajectory.getState(closestState1Idx)
+                closestState2 = self._currentTrajectory.getState(closestState2Idx)
+
+                fieldRelativeSpeeds = ChassisSpeeds.fromRobotRelativeSpeeds(currentSpeeds, currentPose.rotation())
+                currentHeading = Rotation2d(fieldRelativeSpeeds.vx, fieldRelativeSpeeds.vy)
+                headingError = currentHeading - closestState1.heading
+                onHeading = math.hypot(currentSpeeds.vx, currentSpeeds.vy) < 1.0 or abs(headingError.degrees()) < 30
+
+                # Replan the path if we are more than 0.25m away or our heading is off
+                if not onHeading or (
+                        self._replanningConfig.enableInitialReplanning and currentPose.translation().distance(
+                        closestState1.positionMeters) > 0.25):
+                    self._currentPath = self._currentPath.replan(currentPose, currentSpeeds)
+                    self._currentTrajectory = PathPlannerTrajectory(self._currentPath, currentSpeeds,
+                                                                    currentPose.rotation())
+
+                    self._timeOffset = 0.0
+                else:
+                    d = closestState1.positionMeters.distance(closestState2.positionMeters)
+                    t = (currentPose.translation().distance(closestState1.positionMeters)) / d
+
+                    self._timeOffset = floatLerp(closestState1.timeSeconds, closestState2.timeSeconds, t)
+
+                PathPlannerLogging.logActivePath(self._currentPath)
+                PPLibTelemetry.setCurrentPath(self._currentPath)
+
+            self._timer.reset()
+            self._timer.start()
+
+        if self._currentTrajectory is not None:
+            targetState = self._currentTrajectory.sample(self._timer.get() + self._timeOffset)
+
+            if self._replanningConfig.enableDynamicReplanning:
+                previousError = abs(self._controller.getPositionalError())
+                currentError = currentPose.translation().distance(targetState.positionMeters)
+
+                if currentError >= self._replanningConfig.dynamicReplanningTotalErrorThreshold or currentError - previousError >= self._replanningConfig.dynamicReplanningErrorSpikeThreshold:
+                    replanned = self._currentPath.replan(currentPose, currentSpeeds)
+                    self._currentTrajectory = PathPlannerTrajectory(replanned, currentSpeeds, currentPose.rotation())
+                    PathPlannerLogging.logActivePath(replanned)
+                    PPLibTelemetry.setCurrentPath(replanned)
+
+                    self._timer.reset()
+                    self._timeOffset = 0.0
+                    targetState = self._currentTrajectory.sample(0.0)
+
+            # Set the target rotation to the starting rotation if we have not yet traveled the rotation
+            # delay distance
+            if currentPose.translation().distance(self._startingPose.translation()) < self._rotationDelayDistance:
+                targetState.targetHolonomicRotation = self._startingPose.rotation()
+
+            targetSpeeds = self._controller.calculateRobotRelativeSpeeds(currentPose, targetState)
+
+            currentVel = math.hypot(currentSpeeds.vx, currentSpeeds.vy)
+
+            PPLibTelemetry.setCurrentPose(currentPose)
+            PathPlannerLogging.logCurrentPose(currentPose)
+
+            if self._controller.isHolonomic():
+                PPLibTelemetry.setTargetPose(targetState.getTargetHolonomicPose())
+                PathPlannerLogging.logTargetPose(targetState.getTargetHolonomicPose())
+            else:
+                PPLibTelemetry.setTargetPose(targetState.getDifferentialPose())
+                PathPlannerLogging.logTargetPose(targetState.getDifferentialPose())
+
+            PPLibTelemetry.setVelocities(currentVel, targetState.velocityMps, currentSpeeds.omega, targetSpeeds.omega)
+            PPLibTelemetry.setPathInaccuracy(self._controller.getPositionalError())
+
+            self._output(targetSpeeds)
+
+    def isFinished(self) -> bool:
+        if self._targetPath is not None:
+            currentPose = self._poseSupplier()
+            currentSpeeds = self._speedsSupplier()
+
+            currentVel = math.hypot(currentSpeeds.vx, currentSpeeds.vy)
+            stoppingDistance = (currentVel ** 2) / (2 * self._constraints.maxAccelerationMpsSq)
+
+            return currentPose.translation().distance(self._targetPath.getPoint(0).position) <= stoppingDistance
+
+        if self._currentTrajectory is not None:
+            return self._timer.hasElapsed(self._currentTrajectory.getTotalTimeSeconds() - self._timeOffset)
+
+        return False
+
+    def end(self, interrupted: bool):
+        self._timer.stop()
+
+        # Only output 0 speeds when ending a path that is supposed to stop, this allows interrupting
+        # the command to smoothly transition into some auto-alignment routine
+        if not interrupted and self._goalEndState.velocity < 0.1:
+            self._output(ChassisSpeeds())
+
+        PathPlannerLogging.logActivePath(None)
+
+
+class PathfindHolonomic(PathfindingCommand):
+    def __init__(self, constraints: PathConstraints, pose_supplier: Callable[[], Pose2d],
+                 speeds_supplier: Callable[[], ChassisSpeeds], output_robot_relative: Callable[[ChassisSpeeds], None],
+                 config: HolonomicPathFollowerConfig, *requirements: Subsystem,
+                 rotation_delay_distance: float = 0.0, target_path: PathPlannerPath = None, target_pose: Pose2d = None,
+                 goal_end_vel: float = 0):
+        super().__init__(constraints, pose_supplier, speeds_supplier, output_robot_relative,
+                         PPHolonomicDriveController(config.translationConstants, config.rotationConstants,
+                                                    config.maxModuleSpeed, config.driveBaseRadius, config.period),
+                         config.replanningConfig, *requirements, rotation_delay_distance=rotation_delay_distance,
+                         target_path=target_path,
+                         target_pose=target_pose, goal_end_vel=goal_end_vel)
+
+
+class PathfindRamsete(PathfindingCommand):
+    def __init__(self, constraints: PathConstraints, pose_supplier: Callable[[], Pose2d],
+                 speeds_supplier: Callable[[], ChassisSpeeds], output_robot_relative: Callable[[ChassisSpeeds], None],
+                 replanning_config: ReplanningConfig, *requirements: Subsystem,
+                 target_path: PathPlannerPath = None, target_pose: Pose2d = None,
+                 goal_end_vel: float = 0):
+        super().__init__(constraints, pose_supplier, speeds_supplier, output_robot_relative,
+                         PPRamseteController(), replanning_config, *requirements, target_path=target_path,
+                         target_pose=target_pose, goal_end_vel=goal_end_vel)
+
+
+class PathfindLTV(PathfindingCommand):
+    def __init__(self, constraints: PathConstraints, pose_supplier: Callable[[], Pose2d],
+                 speeds_supplier: Callable[[], ChassisSpeeds], output_robot_relative: Callable[[ChassisSpeeds], None],
+                 qelems: Tuple[float, float, float], relems: Tuple[float, float], dt: float,
+                 replanning_config: ReplanningConfig, *requirements: Subsystem,
+                 target_path: PathPlannerPath = None, target_pose: Pose2d = None,
+                 goal_end_vel: float = 0):
+        super().__init__(constraints, pose_supplier, speeds_supplier, output_robot_relative,
+                         PPLTVController(qelems, relems, dt), replanning_config, *requirements,
+                         target_path=target_path, target_pose=target_pose, goal_end_vel=goal_end_vel)

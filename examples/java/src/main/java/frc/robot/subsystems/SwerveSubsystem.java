a6a7d1502d6c75d2219c92a241a0479ea24f7eb9
Michael Jansen <mjansen4857@gmail.com>
2024-01-09 11:03:18 -0500

    update java example

--- examples/java/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ examples/java/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -53,7 +54,18 @@ public class SwerveSubsystem extends SubsystemBase {
       this::resetPose, 
       this::getSpeeds, 
       this::driveRobotRelative, 
-      Constants.Swerve.pathFollowerConfig, 
+      Constants.Swerve.pathFollowerConfig,
+      () -> {
+          // Boolean supplier that controls when the path will be mirrored for the red alliance
+          // This will flip the path being followed to the red side of the field.
+          // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+          var alliance = DriverStation.getAlliance();
+          if (alliance.isPresent()) {
+              return alliance.get() == DriverStation.Alliance.Red;
+          }
+          return false;
+      },
       this
     );
 

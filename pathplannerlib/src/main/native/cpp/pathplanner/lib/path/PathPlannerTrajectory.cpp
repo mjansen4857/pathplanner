b5d605320e13dc7b53901b08f5045e7b1a2fb61a
Michael Jansen <mjansen4857@gmail.com>
2024-01-15 23:19:13 -0500

    formatting

--- pathplannerlib/src/main/native/cpp/pathplanner/lib/path/PathPlannerTrajectory.cpp
+++ pathplannerlib/src/main/native/cpp/pathplanner/lib/path/PathPlannerTrajectory.cpp
@@ -92,7 +92,8 @@ std::vector<PathPlannerTrajectory::State> PathPlannerTrajectory::generateStates(
 				t = 0.0;
 			}
 
-			state.targetHolonomicRotation = GeometryUtil::rotationLerp(prevRotationTargetRot, nextTarget.getTarget(), t);
+			state.targetHolonomicRotation = GeometryUtil::rotationLerp(
+					prevRotationTargetRot, nextTarget.getTarget(), t);
 		}
 
 		state.position = path->getPoint(i).position;

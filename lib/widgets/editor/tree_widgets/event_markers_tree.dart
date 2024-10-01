dadda574e28dfab74be3d21b4577e4f8834e3fa2
Michael Jansen <mjansen4857@gmail.com>
2024-10-01 14:15:55 -0400

    add zoned events

--- lib/widgets/editor/tree_widgets/event_markers_tree.dart
+++ lib/widgets/editor/tree_widgets/event_markers_tree.dart
@@ -8,7 +8,6 @@ import 'package:pathplanner/widgets/editor/info_card.dart';
 import 'package:pathplanner/widgets/editor/tree_widgets/commands/command_group_widget.dart';
 import 'package:pathplanner/widgets/editor/tree_widgets/item_count.dart';
 import 'package:pathplanner/widgets/editor/tree_widgets/tree_card_node.dart';
-import 'package:pathplanner/widgets/number_text_field.dart';
 import 'package:pathplanner/widgets/renamable_title.dart';
 import 'package:undo/undo.dart';
 
@@ -105,9 +104,6 @@ class _EventMarkersTreeState extends State<EventMarkersTree> {
 
   Widget _buildMarkerCard(int markerIdx) {
     ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final TextEditingController timeController = TextEditingController(
-      text: markers[markerIdx].waypointRelativePos.toStringAsFixed(2),
-    );
 
     return TreeCardNode(
       leading: const Icon(Icons.pin_drop_rounded),
@@ -183,70 +179,107 @@ class _EventMarkersTreeState extends State<EventMarkersTree> {
       elevation: 4.0,
       children: [
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 6.0),
+          padding: const EdgeInsets.symmetric(horizontal: 0.0),
           child: Row(
             children: [
-              const SizedBox(width: 8),
-              SizedBox(
-                width: 100,
-                child: NumberTextField(
-                  initialText:
-                      markers[markerIdx].waypointRelativePos.toStringAsFixed(2),
-                  label: 'Location',
-                  arrowKeyIncrement: 0.1,
-                  onSubmitted: (value) {
-                    if (value != null &&
-                        value >= 0.0 &&
-                        value <= (waypoints.length - 1.0)) {
-                      setState(() {
-                        markers[markerIdx].waypointRelativePos = value;
-                        widget.onPathChangedNoSim?.call();
-                      });
-                    } else {
-                      // Optional: Show error message or handle invalid input
-                    }
-                  },
-                ),
-              ),
-              Expanded(
-                child: Slider(
-                  value: markers[markerIdx].waypointRelativePos.toDouble(),
-                  min: 0.0,
-                  max: waypoints.length - 1.0,
-                  divisions: (waypoints.length - 1) * 20,
-                  label:
-                      markers[markerIdx].waypointRelativePos.toStringAsFixed(2),
-                  onChangeStart: (value) {
-                    _sliderChangeStart = value;
-                  },
-                  onChangeEnd: (value) {
-                    widget.undoStack.add(Change(
-                      _sliderChangeStart,
-                      () {
-                        markers[markerIdx].waypointRelativePos = value;
-                        timeController.text = value.toStringAsFixed(2);
-                        widget.onPathChangedNoSim?.call();
-                      },
-                      (oldValue) {
-                        markers[markerIdx].waypointRelativePos = oldValue;
-                        timeController.text = oldValue.toStringAsFixed(2);
-                        widget.onPathChangedNoSim?.call();
-                      },
-                    ));
-                  },
-                  onChanged: (value) {
-                    setState(() {
-                      markers[markerIdx].waypointRelativePos = value;
-                      timeController.text = value.toStringAsFixed(2);
+              Checkbox(
+                value: markers[markerIdx].isZoned,
+                onChanged: (value) {
+                  widget.undoStack.add(Change(
+                    markers[markerIdx].endWaypointRelativePos,
+                    () {
+                      markers[markerIdx].endWaypointRelativePos =
+                          (value ?? false)
+                              ? markers[markerIdx].waypointRelativePos
+                              : null;
                       widget.onPathChangedNoSim?.call();
-                    });
-                  },
-                ),
+                    },
+                    (oldValue) {
+                      markers[markerIdx].endWaypointRelativePos = oldValue;
+                      widget.onPathChangedNoSim?.call();
+                    },
+                  ));
+                },
+              ),
+              const SizedBox(width: 4),
+              const Text(
+                'Zoned Event',
+                style: TextStyle(fontSize: 15),
               ),
             ],
           ),
         ),
+        Slider(
+          value: markers[markerIdx].waypointRelativePos.toDouble(),
+          secondaryTrackValue:
+              markers[markerIdx].endWaypointRelativePos?.toDouble(),
+          min: 0.0,
+          max: waypoints.length - 1.0,
+          divisions: (waypoints.length - 1) * 20,
+          label: markers[markerIdx].waypointRelativePos.toStringAsFixed(2),
+          onChangeStart: (value) {
+            _sliderChangeStart = value;
+          },
+          onChangeEnd: (value) {
+            widget.undoStack.add(Change(
+              _sliderChangeStart,
+              () {
+                markers[markerIdx].waypointRelativePos = value;
+                widget.onPathChangedNoSim?.call();
+              },
+              (oldValue) {
+                markers[markerIdx].waypointRelativePos = oldValue;
+                widget.onPathChangedNoSim?.call();
+              },
+            ));
+          },
+          onChanged: (value) {
+            if (!markers[markerIdx].isZoned ||
+                value <= markers[markerIdx].endWaypointRelativePos!) {
+              setState(() {
+                markers[markerIdx].waypointRelativePos = value;
+                widget.onPathChangedNoSim?.call();
+              });
+            }
+          },
+        ),
         const SizedBox(height: 12),
+        if (markers[markerIdx].isZoned) ...[
+          Slider(
+            value: markers[markerIdx].endWaypointRelativePos!.toDouble(),
+            min: 0.0,
+            max: waypoints.length - 1.0,
+            divisions: (waypoints.length - 1) * 20,
+            label:
+                markers[markerIdx].endWaypointRelativePos!.toStringAsFixed(2),
+            onChangeStart: (value) {
+              _sliderChangeStart = value;
+            },
+            onChangeEnd: (value) {
+              widget.undoStack.add(Change(
+                _sliderChangeStart,
+                () {
+                  markers[markerIdx].endWaypointRelativePos = value;
+                  widget.onPathChangedNoSim?.call();
+                },
+                (oldValue) {
+                  markers[markerIdx].endWaypointRelativePos = oldValue;
+                  widget.onPathChangedNoSim?.call();
+                },
+              ));
+            },
+            onChanged: (value) {
+              if (value >= markers[markerIdx].waypointRelativePos) {
+                setState(() {
+                  markers[markerIdx].endWaypointRelativePos = value;
+                  widget.onPathChangedNoSim?.call();
+                });
+              }
+            },
+          ),
+          const SizedBox(height: 12),
+        ],
+        const Divider(),
         _buildCommandCard(markerIdx),
       ],
     );

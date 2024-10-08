1343cc4cf31458e4308d2f81854232f11af14121
Michael Jansen <mjansen4857@gmail.com>
2024-10-08 00:53:32 -0400

    missing javadoc

--- pathplannerlib/src/main/java/com/pathplanner/lib/util/JSONUtil.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/util/JSONUtil.java
@@ -3,6 +3,7 @@ package com.pathplanner.lib.util;
 import edu.wpi.first.math.geometry.Translation2d;
 import org.json.simple.JSONObject;
 
+/** Utility class for creating different objects from JSON */
 public class JSONUtil {
   /**
    * Create a Translation2d from a json object containing x and y fields

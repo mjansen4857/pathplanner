8abac6ec66e2df04d5d94d2e71ea24c84c50c587
Michael Jansen <mjansen4857@gmail.com>
2024-12-19 11:41:26 -0500

    [cpp] integrate field speeds to interpolate state positions

--- pathplannerlib/src/main/native/cpp/pathplanner/lib/trajectory/PathPlannerTrajectoryState.cpp
+++ pathplannerlib/src/main/native/cpp/pathplanner/lib/trajectory/PathPlannerTrajectoryState.cpp
@@ -18,13 +18,41 @@ PathPlannerTrajectoryState PathPlannerTrajectoryState::interpolate(
 			fieldSpeeds.vx, endVal.fieldSpeeds.vx, t), GeometryUtil::unitLerp(
 			fieldSpeeds.vy, endVal.fieldSpeeds.vy, t), GeometryUtil::unitLerp(
 			fieldSpeeds.omega, endVal.fieldSpeeds.omega, t) };
-	lerpedState.pose = frc::Pose2d(
-			GeometryUtil::translationLerp(pose.Translation(),
-					endVal.pose.Translation(), t),
-			GeometryUtil::rotationLerp(pose.Rotation(), endVal.pose.Rotation(),
-					t));
+
+	lerpedState.heading = heading;
 	lerpedState.linearVelocity = GeometryUtil::unitLerp(linearVelocity,
 			endVal.linearVelocity, t);
+
+	// Integrate the field speeds to get the pose for this interpolated state, since linearly
+	// interpolating the pose gives an inaccurate result if the speeds are changing between states
+	units::meter_t lerpedXPos = pose.Translation().X();
+	units::meter_t lerpedYPos = pose.Translation().Y();
+	units::second_t intTime = time + 0.01_s;
+	while (true) {
+		double intT = ((intTime - time) / (lerpedState.time - time))();
+		units::meters_per_second_t intLinearVel = GeometryUtil::unitLerp(
+				linearVelocity, lerpedState.linearVelocity, intT);
+		units::meters_per_second_t intVX = intLinearVel
+				* lerpedState.heading.Cos();
+		units::meters_per_second_t intVY = intLinearVel
+				* lerpedState.heading.Sin();
+
+		if (intTime >= lerpedState.time - 0.01_s) {
+			units::second_t dt = lerpedState.time - intTime;
+			lerpedXPos += intVX * dt;
+			lerpedYPos += intVY * dt;
+			break;
+		}
+
+		lerpedXPos += intVX * 0.01_s;
+		lerpedYPos += intVY * 0.01_s;
+
+		intTime += 0.01_s;
+	}
+
+	lerpedState.pose = frc::Pose2d(lerpedXPos, lerpedYPos,
+			GeometryUtil::rotationLerp(pose.Rotation(), endVal.pose.Rotation(),
+					t));
 	lerpedState.feedforwards = feedforwards.interpolate(endVal.feedforwards, t);
 
 	return lerpedState;

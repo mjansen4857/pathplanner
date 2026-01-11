b0d507d6e296a960e6ced8265521225ff4c65842
Michael Jansen <mjansen4857@gmail.com>
2026-01-11 00:26:03 -0500

    double oops

--- pathplannerlib/src/main/java/com/pathplanner/lib/util/FlippingUtil.java
+++ pathplannerlib/src/main/java/com/pathplanner/lib/util/FlippingUtil.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.util.Units;
 
 /** Utility class for flipping positions/rotations to the other side of the field */
 public class FlippingUtil {
